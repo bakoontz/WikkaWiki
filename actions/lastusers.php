1c88aebfd4f3612625bbd24fc8f801968989e62a
Nils Lindenberg <nilslindenberg@wikkawiki.org>
2005-12-13 13:36:47 +0000

    fixed #31 - wrong closing-tag for table headers

--- actions/lastusers.php
+++ actions/lastusers.php
@@ -1,4 +1,9 @@
 <?php
+// ***** CONSTANTS section *****
+define('NAME', "Name");
+define('OWNED_PAGES', "Owned Pages");
+define('SIGNUP_DATE_TIME', "Signup Date/Time");
+// ***** END CONSTANTS section *****
 
 if ($stat===0) $limit = 1000;
 else $limit = 100;
@@ -10,16 +15,16 @@ $last_users = $this->LoadAll("select name, signuptime from ".$this->config["tabl
 
 $htmlout = "<table width=\"50%\" border=\"0\" cellpadding=\"3%\">\n".
   "  <tr>\n".
-    "    <th><u>Name</u></td>\n".
-    "    <th><u>Owned Pages</u></td>\n".
-    "    <th><u>Signup Date/Time</u></td>\n".
+    "    <th><u>".NAME."</u></th>\n".
+    "    <th><u>".OWNED_PAGES."</u></th>\n".
+    "    <th><u>".SIGNUP_DATE_TIME."</u></th>\n".
   "  </tr>\n";
 
-foreach($last_users as $user) 
-{ 
+foreach($last_users as $user)
+{
  $htmlout .= "  <tr>\n";
  if ($stat!=="0") $num = $this->LoadSingle("select count(*) as n from ".$this->config["table_prefix"]."pages where owner='".$user["name"]."' AND latest = 'Y'");
- $htmlout .= "    <td>".$this->Link($user["name"])."</td>\n    <td>".($stat!=="0"?" . . . . . (".$num["n"].")":"")."</td>\n    <td>(".$user["signuptime"].")</td>\n"; 
+ $htmlout .= "    <td>".$this->Link($user["name"])."</td>\n    <td>".($stat!=="0"?" . . . . . (".$num["n"].")":"")."</td>\n    <td>(".$user["signuptime"].")</td>\n";
  $htmlout .= "  </tr>\n";
 }
 
