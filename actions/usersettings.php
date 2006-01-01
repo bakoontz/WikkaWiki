7a8b6212fd5953895efd667a1091f4e198d14e28
Dario Taraborelli <dartar@wikkawiki.org>
2006-01-01 16:39:37 +0000

    Usersettings - preliminary cleanup: adding defaults and i18n strings

--- actions/usersettings.php
+++ actions/usersettings.php
@@ -1,8 +1,52 @@
 <?php
+// defaults
+define('PASSWORD_MIN_LENGTH', "5");
+define('VALID_EMAIL_PATTERN', "/^.+?\@.+?\..+$/"); //TODO: Use central regex library
+
+// i18n strings
+define('USER_LOGGED_OUT', "You are now logged out.");
+define('USER_SETTINGS_STORED', "User settings stored!");
+define('ERROR_NO_BLANK', "Sorry, blanks are not permitted in the password.");
+define('ERROR_PASSWORD_TOO_SHORT', "Sorry, the password must contain at least %s characters.");
+define('PASSWORD_CHANGED', "Password changed!");
+define('ERROR_OLD_PASSWORD_WRONG', "The old password you entered is wrong.");
+define('USER_EMAIL_LABEL', "Your email address:");
+define('DOUBLECLICK_LABEL', "Doubleclick Editing:");
+define('SHOW_COMMENTS_LABEL', "Show comments by default:");
+define('RECENTCHANGES_DISPLAY_LIMIT_LABEL', "RecentChanges display limit:");
+define('PAGEREVISION_LIST_LIMIT_LABEL', "Page revisions list limit:");
+define('UPDATE_SETTINGS_INPUT', "Update Settings");
+define('CHANGE_PASSWORD_LABEL', "Change your password:");
+define('CURRENT_PASSWORD_LABEL', "Your current password:");
+define('NEW_PASSWORD_LABEL', "Your new password:");
+define('CHANGE_BUTTON_LABEL', "Change");
+define('REGISTER_BUTTON_LABEL', "Register");
+define('QUICK_LINKS', "See a list of pages you own (MyPages) and pages you've edited (MyChanges).");
+define('ERROR_WRONG_PASSWORD', "Sorry, you entered the wrong password.");
+define('ERROR_EMPTY_USERNAME', "Please fill in your user name.");
+define('ERROR_RESERVED_PAGENAME', "Sorry, this name is reserved for a page. Please choose a different name.");
+define('ERROR_WIKINAME', "User name must be ##\"\"WikiName\"\"## formatted, e.g. ##\"\"JohnDoe\"\"##.");
+define('ERROR_EMPTY_PASSWORD', "Please fill in a password.");
+define('ERROR_EMPTY_CONFIRMATION_PASSWORD', "You must confirm your password to register a new account.");
+define('ERROR_PASSWORD_MATCH', "Passwords didn't match.");
+define('ERROR_EMAIL_ADDRESS_REQUIRED', "You must specify an email address.");
+define('ERROR_INVALID_EMAIL_ADDRESS', "That doesn't quite look like an email address.");
+define('REGISTERED_USER_LOGIN_LABEL', "If you're already a registered user, log in here!");
+define('REGISTER_HEADING', "===Login/Register===");
+define('WIKINAME_LABEL', "Your <abbrv title=\"A WikiName is formed by two or more capitalized words without space, e.g. JohnDoe\">WikiName</abbrv>:");
+define('PASSWORD_LABEL', "Password (min. %s chars):");
+define('LOGIN_BUTTON_LABEL', "Login");
+define('NEW_USER_REGISTER_LABEL', "Stuff you only need to fill in when you're logging in for the first time (and thus signing up as a new user on this site).");
+define('CONFIRM_PASSWORD_LABEL', "Confirm password:");
+define('RETRIEVE_PASSWORD_HEADING', "===Forgot your password?===");
+define('RETRIEVE_PASSWORD_MESSAGE', "Log in here with the temporary password. --- If you need a temporary password, click [[PasswordForgotten here]].");
+define('TEMP_PASSWORD_LABEL', "Your temp password:");
+
+// is user logging out?
 if (isset($_REQUEST["action"]) && ($_REQUEST["action"] == "logout"))
 {
 	$this->LogoutUser();
-	$this->Redirect($this->href(), "You are now logged out.");
+	$this->Redirect($this->href(), USER_LOGGED_OUT);
 }
 else if ($user = $this->GetUser())
 {
@@ -21,25 +65,25 @@ else if ($user = $this->GetUser())
 		$this->SetUser($this->LoadUser($user["name"]));
 		
 		// forward
-		$this->Redirect($this->href(), "User settings stored!");
+		$this->Redirect($this->href(), USER_SETTINGS_STORED);
 	}
 	
 	if (isset($_REQUEST["action"]) && ($_REQUEST["action"] == "changepass"))
 	{
 			// check password
 			$password = $_POST["password"];			
-                        if (preg_match("/ /", $password)) $passerror = "Sorry, blanks are not permitted in the password.";
-			else if (strlen($password) < 5) $passerror = "Tsk tsk, the password is much too short...";
+                        if (preg_match("/ /", $password)) $passerror = ERROR_NO_BLANK;
+			else if (strlen($password) < PASSWORD_MIN_LENGTH) $passerror = sprintf(ERROR_PASSWORD_TOO_SHORT, PASSWORD_MIN_LENGTH);
 			else if (($user["password"] == md5($_POST["oldpass"])) || ($user["password"] == $_POST["oldpass"]))
 			{
 				$this->Query("update ".$this->config["table_prefix"]."users set "."password = md5('".mysql_real_escape_string($password)."') "."where name = '".$user["name"]."'");				
 				$user["password"]=md5($password);
 				$this->SetUser($user);
-				$this->Redirect($this->href(), "Password changed.");
+				$this->Redirect($this->href(), PASSWORD_CHANGED);
 			}
 			else
 			{
-				$passerror = "The old password you entered is wrong.";
+				$passerror = ERROR_OLD_PASSWORD_WRONG;
 			}
 	}
 
@@ -54,28 +98,28 @@ else if ($user = $this->GetUser())
 			<td>Hello, <?php echo $this->Link($user["name"]) ?>!</td>
 		</tr>
 		<tr>
-			<td align="right">Your email address:</td>
+			<td align="right"><?php echo USER_EMAIL_LABEL ?></td>
 			<td><input name="email" value="<?php echo $this->htmlspecialchars_ent($user["email"]) ?>" size="40" /></td>
 		</tr>
 		<tr>
-			<td align="right">Doubleclick Editing:</td>
+			<td align="right"><?php echo DOUBLECLICK_LABEL ?></td>
 			<td><input type="hidden" name="doubleclickedit" value="N" /><input type="checkbox" name="doubleclickedit" value="Y" <?php echo $user["doubleclickedit"] == "Y" ? "checked=\"checked\"" : "" ?> /></td>
 		</tr>
 		<tr>
-			<td align="right">Show comments by default:</td>
+			<td align="right"><?php echo SHOW_COMMENTS_LABEL ?></td>
 			<td><input type="hidden" name="show_comments" value="N" /><input type="checkbox" name="show_comments" value="Y" <?php echo $user["show_comments"] == "Y" ? "checked=\"checked\"" : "" ?> /></td>
 		</tr>
 		<tr>
-			<td align="right">RecentChanges display limit:</td>
+			<td align="right"><?php echo RECENTCHANGES_DISPLAY_LIMIT_LABEL ?></td>
 			<td><input name="changescount" value="<?php echo $this->htmlspecialchars_ent($user["changescount"]) ?>" size="40" /></td>
 		</tr>
 		<tr>
-			<td align="right">Page revisions list limit:</td>
+			<td align="right"><?php echo PAGEREVISION_LIST_LIMIT_LABEL ?></td>
 			<td><input name="revisioncount" value="<?php echo $this->htmlspecialchars_ent($user["revisioncount"]) ?>" size="40" /></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" value="Update Settings" /> <input type="button" value="Logout" onclick="document.location='<?php echo $this->href("", "", "action=logout"); ?>'" /></td>
+			<td><input type="submit" value="<?php echo UPDATE_SETTINGS_INPUT ?>" /> <input type="button" value="Logout" onclick="document.location='<?php echo $this->href("", "", "action=logout"); ?>'" /></td>
 		</tr>
 	</table>
 	<?php
@@ -86,38 +130,37 @@ else if ($user = $this->GetUser())
 	<input type="hidden" name="action" value="changepass" />
 	<table>
 		<tr>
-			<td align="left"><b>Change your password:</b></td>
+			<td align="left"><b><?php echo CHANGE_PASSWORD_LABEL ?></b></td>
 			<td><br /><br />&nbsp;</td>
 		</tr>
 		<?php
 		if (isset($passerror))
 		{
-			print("<tr><td></td><td><div class=\"error\">".$this->Format($passerror)."</div></td></tr>\n");
+			print('<tr><td></td><td><em class="error">'.$this->Format($passerror).'</em></td></tr>'."\n");
 		}
 		?>
 		<tr>
-			<td align="left">Your current password:</td>
+			<td align="left"><?php echo CURRENT_PASSWORD_LABEL ?></td>
 			<td><input type="password" name="oldpass" size="40" /></td>
 		</tr>
 		<tr>
-			<td align="left">Your new password:</td>
+			<td align="left"><?php echo NEW_PASSWORD_LABEL ?></td>
 			<td><input type="password" name="password" size="40" /></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" value="Change" size="40" /></td>
+			<td><input type="submit" value="<?php echo CHANGE_BUTTON_LABEL ?>" size="40" /></td>
 
 		</tr>
 	</table>
 	<br />
-	See a list of pages you own (<a href="<?php echo $this->href("", "MyPages"); ?>">MyPages</a>) and pages you've edited (<a href="<?php echo $this->href("", "MyChanges"); ?>">MyChanges</a>).<br />
+	<?php echo $this->Format(QUICK_LINKS); ?>
 	<?php
 	print($this->FormClose());
 }
 else
 {
 	// user is not logged in
-
 	print "<script type=\"text/javascript\"><!-- \nfunction hov(loc,cls){ \n    if(loc.className) loc.className=cls;\n}\n //-->\n</script>\n";
 
 	// is user trying to log in or register?
@@ -134,7 +177,7 @@ else
 			}
 			else
 			{
-				$error = "Wrong password!";
+				$error = ERROR_WRONG_PASSWORD;
 			}
 		}
 		// otherwise, create new account
@@ -146,13 +189,16 @@ else
 			$confpassword = $_POST["confpassword"];
 
 			// check if name is WikiName style
-			if ($this->ExistsPage($name)) $error = 'Sorry, this ""WikiName"" is reserved for a page. Please choose a different name.';
-			elseif (!$this->IsWikiName($name)) $error = "User name must be WikiName formatted!";
-			elseif (!$email) $error = "You must specify an email address.";
-			elseif (!preg_match("/^.+?\@.+?\..+$/", $email)) $error = "That doesn't quite look like an email address.";
-			elseif ($confpassword != $password) $error = "Passwords didn't match.";
-			elseif (preg_match("/ /", $password)) $error = "Spaces aren't allowed in passwords.";
-			elseif (strlen($password) < 5) $error = "Password too short.";
+			if (strlen($name)==0) $error = ERROR_EMPTY_USERNAME;
+			elseif (!$this->IsWikiName($name)) $error = ERROR_WIKINAME;
+			elseif ($this->ExistsPage($name)) $error = ERROR_RESERVED_PAGENAME;
+			elseif (strlen($password)==0) $error = ERROR_EMPTY_PASSWORD;
+			elseif (preg_match("/ /", $password)) $error = ERROR_NO_BLANK;
+			elseif (strlen($password) < PASSWORD_MIN_LENGTH) $error = sprintf(ERROR_PASSWORD_TOO_SHORT, PASSWORD_MIN_LENGTH);
+			elseif (strlen($confpassword)==0) $error = ERROR_EMPTY_CONFIRMATION_PASSWORD;
+			elseif ($confpassword != $password) $error = ERROR_PASSWORD_MATCH;
+			elseif (!$email) $error = ERROR_EMAIL_ADDRESS_REQUIRED;
+			elseif (!preg_match(VALID_EMAIL_PATTERN, $email)) $error = ERROR_INVALID_EMAIL_ADDRESS;
 			else
 			{
 				$this->Query("insert into ".$this->config["table_prefix"]."users set ".
@@ -171,9 +217,9 @@ else
 	}
 	elseif  (isset($_REQUEST["action"]) && ($_REQUEST["action"] == "updatepass"))
 	{
-        	// check if name is WikkiName style
+        	// check if name is WikiName style
         	$name = trim($_POST["yourname"]);
-        	if (!$this->IsWikiName($name)) $newerror = "You have entered an incorrect or non-existent wikiname. The wikiname must be written in wikistyle, e.g: \"\"WikkaName.\"\"";
+        	if (!$this->IsWikiName($name)) $newerror = ERROR_WIKINAME;
    
         	// if user name already exists, check password
         	elseif ($existingUser = $this->LoadUser($_POST["yourname"]))   
@@ -185,7 +231,7 @@ else
                 }
                 else
                {
-                      	$newerror = "Sorry, you entered the wrong password.";
+                      	$newerror = ERROR_WRONG_PASSWORD;
                }
     }
 	
@@ -193,43 +239,46 @@ else
 	?>
 	<input type="hidden" name="action" value="login" />
 	<table>
+   	<tr> 
+   		<td colspan="2"><?php echo $this->Format(REGISTER_HEADING) ?></td><td></td>
+   	</tr>
 		<tr>
 			<td align="right"></td>
-			<td><?php echo $this->Format("If you're already a registered user, log in here!"); ?></td>
+			<td><?php echo $this->Format(REGISTERED_USER_LOGIN_LABEL); ?></td>
 		</tr>
 		<?php
 		if (isset($error))
 		{
-			print("<tr><td></td><td><div class=\"error\">".$this->Format($error)."</div></td></tr>\n");
+			print('<tr><td></td><td><em class="error">'.$this->Format($error).'</em></td></tr>'."\n");
 		}
 		?>
 		<tr>
-			<td align="right">Your WikiName:</td>
+			<td align="right"><?php echo WIKINAME_LABEL ?></td>
 			<td><input name="name" size="40" value="<?php if (isset($name)) echo $name; ?>" /></td>
 		</tr>
 		<tr>
-			<td align="right">Password (5+ chars):</td>
+			<td align="right"><?php echo sprintf(PASSWORD_LABEL, PASSWORD_MIN_LENGTH) ?></td>
 			<td><input type="password" name="password" size="40" /></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" value="Login" size="40" /></td>
+			<td><input type="submit" value="<?php echo LOGIN_BUTTON_LABEL ?>" size="40" /></td>
 		</tr>
 		<tr>
 			<td align="right"></td>
-			<td width="500"><?php echo $this->Format("Stuff you only need to fill in when you're logging in for the first time (and thus signing up as a new user on this site)."); ?></td>
+			<td width="500"><?php echo $this->Format(NEW_USER_REGISTER_LABEL); ?></td>
 		</tr>
 		<tr>
-			<td align="right">Confirm password:</td>
+			<td align="right"><?php echo CONFIRM_PASSWORD_LABEL ?></td>
 			<td><input type="password" name="confpassword" size="40" /></td>
 		</tr>
 		<tr>
-			<td align="right">Email address:</td>
+			<td align="right"><?php echo USER_EMAIL_LABEL ?></td>
 			<td><input name="email" size="40" value="<?php if (isset($email)) echo $email; ?>" /></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" value="Register" size="40" /></td>
+			<td><input type="submit" value="<?php echo REGISTER_BUTTON_LABEL ?>" size="40" /></td>
 		</tr>
 	</table>
 	<?php
@@ -239,29 +288,29 @@ else
 	<input type="hidden" name="action" value="updatepass" />
 	<table>
       	<tr> 
-         		<td colspan="2"><br /><hr /><h4>Forget your password?</h4></td><td></td>
+         		<td colspan="2"><br /><hr /><?php echo $this->Format(RETRIEVE_PASSWORD_HEADING) ?></td><td></td>
          	</tr>
 		<tr>
 			<td align="left"></td>
-			<td>Log in here with the temporary password. <br />If you need a temporary password, click <?php echo $this->Format("[[PasswordForgotten here]]") ?></td>
+			<td><?php echo $this->Format(RETRIEVE_PASSWORD_MESSAGE) ?></td>
 		</tr>
             <?php   
             if (isset($newerror))
             {	
-            	print("<tr><td></td><td><div class=\"error\">".$this->Format($newerror)."</div></td></tr>\n");
+            	print('<tr><td></td><td><em class="error">'.$this->Format($newerror).'</em></td></tr>'."\n");
 		}	
             ?>
 		<tr>
-			<td align="left">Your WikiName:</td>
+			<td align="right"><?php echo WIKINAME_LABEL ?></td>
 			<td><input name="yourname" value="<?php if (isset($_POST["yourname"])) echo $_POST["yourname"]; ?>" size="40" /></td>
 		</tr>
 		<tr>
-			<td align="left">Your temp password:</td>
+			<td align="right"><?php echo TEMP_PASSWORD_LABEL ?></td>
 			<td><input name="temppassword" size="40" /></td>
 		</tr>
 		<tr>
 			<td></td>
-			<td><input type="submit" value="Login" size="40" /></td>
+			<td><input type="submit" value="<?php echo LOGIN_BUTTON_LABEL ?>" size="40" /></td>
 		</tr>
 	   </table>
 	   <?php
