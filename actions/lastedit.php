3f99b0e7b3d42dcc4903089e6223cabaa4298b3b
Dario Taraborelli <dartar@wikkawiki.org>
2006-01-09 12:47:46 +0000

    Lastedit action: fixeduse a phpdoc header; added a bunch of configurable defaults and i18n strings; more compact code using switch.

--- actions/lastedit.php
+++ actions/lastedit.php
@@ -1,36 +1,71 @@
 <?php
+/**
+ * Display a box with information on the last edit.
+ *
+ * @package		Actions
+ * @name			Lastedit
+ *
+ * @author		{@link http://wikka.jsnx.com/DarTar Dario Taraborelli} (first draft)
+ * @author		{@link http://wikka.jsnx.com/MinusF MinusF} (code cleanup and validation)
+ * @version		0.2
+ * @since		Wikka 1.1.6.X
+ *
+ * @input		integer  $show  optional: amount of details to be displayed;
+ *				default: 3
+ *				0: show user only
+ *				1: show user and notes (if available)
+ *				2: show user, notes (if available) and date
+ *				3: show user, notes (if available), date and quickdiff link
+ * @output		box with lastedit information
+ *
+ * @todo		- make date/time format system-configurable;
+ *			- use FormatUser() method to render author name;
+ */
 
-// Prints short infos on last edit
-// Uses parameter: show
-// 0 show user only
-// 1 show user and notes
-// 2 show user, notes, date
-// 3 show user, notes, date and quickdiff link
+// defaults
+define('DEFAULT_SHOW', '3');
+define('DATE_FORMAT', 'D, d M Y'); #TODO make this system-configurable
+define('TIME_FORMAT', 'H:i T'); #TODO make this system-configurable
 
-// set default
-if ($show == "") {$show="1";}
+// style
+define('LASTEDIT_BOX', 'lastedit');
+define('LASTEDIT_NOTES', 'lastedit_notes');
 
-if ($this->method == "show") {
+// i18n strings
+define('ANONYMOUS_USER', 'anonymous');
+define('LASTEDIT_MESSAGE', 'Last edited by %s');
+define('DIFF_LINK_TITLE', 'Show differences from last revision');
+
+if (!isset($show)) 
+{
+    $show = DEFAULT_SHOW;
+}
+
+if ($this->method == 'show') 
+{
 	$page = $this->page;
-	$pagetag = $page["tag"];
-	$user = ($this->LoadUser($page["user"]))? $this->Link($page["user"]) : "anonymous";
+	$pagetag = $page['tag'];
+	$user = ($this->LoadUser($page['user'])) ? $this->Link($page['user']) : ANONYMOUS_USER;
 
-	if (!($show == 0)) {
-		$note = ($page["note"])? ":<br/><span class=\"notes\">".$page["note"]."</span>" : "";
-	}
+	switch($show)
+	{
+		case 3:
+		$oldpage = $this->LoadSingle("SELECT * FROM ".$this->config['table_prefix']."pages WHERE tag='".$this->GetPageTag()."' AND latest = 'N' ORDER BY time desc LIMIT 1");
+		$newid = $page['id'];
+		$oldid = $oldpage['id'];
+		$difflink = ' [<a title="'.DIFF_LINK_TITLE.'" href="'.$this->Href('diff', $pagetag, 'a='.$page['id'].'&amp;b='.$oldpage['id'].'&amp;fastdiff=1').'">diff</a>]';
 
-	if ($show == 2 || $show == 3) {
-		list($day, $time) = explode(" ", $page["time"]);
-		$dateformatted = date("D, d M Y", strtotime($day));
-		$timeformatted = date("H:i T", strtotime($page["time"]));
+		case 2:
+		list($day, $time) = explode(' ', $page['time']);
+		$dateformatted = date(DATE_FORMAT, strtotime($day));
+		$timeformatted = date(TIME_FORMAT, strtotime($page['time']));
+
+		case 1:
+		$note = ($page['note']) ? ':<br/><span class="'.LASTEDIT_NOTES.'">'.
+		$page['note'].'</span>' : '';
+
+		default:	
 	}
-	if ($show == 3) {
-		$oldpage = $this->LoadSingle("SELECT * FROM ".$this->config["table_prefix"]."pages WHERE tag='".$this->GetPageTag()."' AND latest = 'N' ORDER BY time desc LIMIT 1");
-		$newid = $page["id"];
-		$oldid = $oldpage["id"];
-		$difflink = " [<a title=\"Show differences from last version\" href=\"".$this->Href("diff", $pagetag, "a=".$page["id"]."&b=".$oldpage["id"]."&fastdiff=1")."\">diff</a>]";
-	}
-	$output = "<div class=\"lastedit\">Last edited by ".$user.$note."<br /> ".$dateformatted." ".$timeformatted.$difflink."</div>";
-	print $output;
+	echo '<div class="'.LASTEDIT_BOX.'">'.sprintf(LASTEDIT_MESSAGE, $user).$note.'<br /> '.$dateformatted.' '.$timeformatted.$difflink.'</div>';
 }
 ?>
