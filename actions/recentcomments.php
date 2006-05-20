da590fbeb2121df90e3cb8a2a730ee3af50527d7
Dario Taraborelli <dartar@wikkawiki.org>
2006-05-20 00:07:06 +0000

    recentcomments/recentlycommented: preliminary code cleanup, i18n, use of blockquotes, check for read permissions - closes #87.

--- actions/recentcomments.php
+++ actions/recentcomments.php
@@ -1,32 +1,70 @@
 <?php
+/**
+ * Display a list of recent comments.
+ *
+ * @package		Actions
+ * @name			RecentComments
+ *
+ * @author		{@link http://wikkawiki.org/DarTar Dario Taraborelli} (preliminary code cleanup)
+ * @author		{@link http://wikkawiki.org/NickDamoulakis Nick Damoulakis} (ACL check)
+ * @since		Wikka 1.1.6.2
+ *
+ * @input		none
+ * @todo			- make datetime format configurable
+ */
+//constants
+if(!defined('COMMENT_DATE_FORMAT')) define('COMMENT_DATE_FORMAT', 'D, d M Y');
+if(!defined('COMMENT_TIME_FORMAT')) define('COMMENT_TIME_FORMAT', 'H:i T');
+if(!defined('COMMENT_SNIPPET_LENGTH')) define('COMMENT_SNIPPET_LENGTH', 120);
 
+//i18n
+if (!defined('RECENT_COMMENTS_HEADING')) define('RECENT_COMMENTS_HEADING', '=====Recent comments=====');
+if (!defined('COMMENT_AUTHOR_DIVIDER')) define ('COMMENT_AUTHOR_DIVIDER', ', comment by ');
+if (!defined('NO_RECENT_COMMENTS')) define ('NO_RECENT_COMMENTS', 'There are no recent comments.');
+if (!defined('NO_READABLE_RECENT_COMMENTS')) define ('NO_READABLE_RECENT_COMMENTS', 'There are no recent comments you can read.');
+$readable = 0;
+
+echo $this->Format(RECENT_COMMENTS_HEADING.' --- ');
 if ($comments = $this->LoadRecentComments())
 {
-	$curday = "";
+	$curday = '';
 	foreach ($comments as $comment)
 	{
-		// day header
-		list($day, $time) = explode(" ", $comment["time"]);
-		if ($day != $curday)
+		if ($this->HasAccess('read', $comment['page_tag']))
 		{
-			$dateformatted = date("D, d M Y", strtotime($day));
+			$readable++;
+			// day header
+			list($day, $time) = explode(' ', $comment['time']);
+			if ($day != $curday)
+			{
+				$dateformatted = date(COMMENT_DATE_FORMAT, strtotime($day));
+	
+				if ($curday)
+				{
+					echo "<br />\n";
+				}
+				echo '<strong>'.$dateformatted.':</strong><br />'."\n";
+				$curday = $day;
+			}
 
-			if ($curday) print("<br />\n");
-			print("<strong>$dateformatted:</strong><br />\n");
-			$curday = $day;
+			$timeformatted = date(COMMENT_TIME_FORMAT, strtotime($comment['time']));
+			$comment_preview = str_replace('<br />', '', $comment['comment']);
+			$comment_preview = substr($comment_preview, 0, COMMENT_SNIPPET_LENGTH);
+			if (strlen($comment['comment']) > COMMENT_SNIPPET_LENGTH)
+			{
+				$comment_preview = $comment_preview.'&#8230;';
+			}
+			// print entry
+			echo '&nbsp;&nbsp;&nbsp;('.$timeformatted.') <a href="'.$this->href('', $comment['page_tag'], 'show_comments=1').'#comment_'.$comment['id'].'">'.$comment['page_tag'].'</a>'.COMMENT_AUTHOR_DIVIDER.$this->Format($comment['user'])."\n<blockquote>".$comment_preview."</blockquote>\n";
 		}
-
-		$timeformatted = date("H:i T", strtotime($comment["time"]));
-		$comment_preview = str_replace("<br />", "", $comment["comment"]);
-		$comment_preview = substr($comment_preview, 0, 125);
-		if (strlen($comment["comment"]) > 120) $comment_preview = $comment_preview.".... ";
-
-		// print entry
-		print("&nbsp;&nbsp;&nbsp;($timeformatted) <a href=\"".$this->href("", $comment["page_tag"], "show_comments=1")."#comment_".$comment["id"]."\">".$comment["page_tag"]."</a> . . . . ".$this->Format($comment["user"])."<br />\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<em>$comment_preview</em><br />\n");
+	}
+	if ($readable == 0)
+	{
+		echo '<em>'.NO_READABLE_RECENT_COMMENTS.'</em>';
 	}
 }
 else
 {
-	print("<em>No recent comments.</em>");
+	echo '<em>'.NO_RECENT_COMMENTS.'</em>';
 }
 ?>
--- actions/recentlycommented.php
+++ actions/recentlycommented.php
@@ -1,41 +1,75 @@
 <?php
+/**
+ * Display a list of recently commented pages.
+ *
+ * @package		Actions
+ * @name			RecentlyCommented
+ *
+ * @author		{@link http://wikkawiki.org/DarTar Dario Taraborelli} (preliminary code cleanup, ACL check)
+ * @since		Wikka 1.1.6.2
+ *
+ * @input		none
+ */
+//constants
+if(!defined('COMMENT_DATE_FORMAT')) define('COMMENT_DATE_FORMAT', 'D, d M Y');
+if(!defined('COMMENT_TIME_FORMAT')) define('COMMENT_TIME_FORMAT', 'H:i T');
+if(!defined('COMMENT_SNIPPET_LENGTH')) define('COMMENT_SNIPPET_LENGTH', 120);
 
+//i18n
+if (!defined('RECENTLY_COMMENTED_HEADING')) define('RECENTLY_COMMENTED_HEADING', '=====Recently commented pages=====');
+if(!defined('ANONYMOUS_COMMENT_AUTHOR')) define('ANONYMOUS_COMMENT_AUTHOR', '(unregistered user)');
+if (!defined('COMMENT_AUTHOR_DIVIDER')) define ('COMMENT_AUTHOR_DIVIDER', ', comment by ');
+if (!defined('NO_RECENTLY_COMMENTED')) define ('NO_RECENTLY_COMMENTED', 'There are no recently commented pages.');
+if (!defined('NO_READABLE_RECENTLY_COMMENTED')) define ('NO_READABLE_RECENTLY_COMMENTED', 'There are no recently commented pages you can read.');
+$readable = 0;
+
+echo $this->Format(RECENTLY_COMMENTED_HEADING.' --- ');
 if ($comments = $this->LoadRecentlyCommented())
 {
-	$curday = "";
+	$curday = '';
 	foreach ($comments as $comment)
 	{
-		// day header
-		list($day, $time) = explode(" ", $comment["time"]);
-		if ($day != $curday)
+		if ($this->HasAccess('read', $comment[page_tag]))
 		{
-			$dateformatted = date("D, d M Y", strtotime($day));
-
-			if ($curday) print("<br />\n");
-			print("<strong>$dateformatted:</strong><br />\n");
-			$curday = $day;
+			$readable++;
+			// day header
+			list($day, $time) = explode(' ', $comment['time']);
+			if ($day != $curday)
+			{
+				$dateformatted = date(COMMENT_DATE_FORMAT, strtotime($day));
+	
+				if ($curday)
+				{
+					echo "<br />\n";
+				}
+				echo '<strong>'.$dateformatted.':</strong><br />'."\n";
+				$curday = $day;
+			}
+			$timeformatted = date(COMMENT_TIME_FORMAT, strtotime($comment['time']));
+			$comment_preview = str_replace('<br />', '', $comment['comment']);
+			if (strlen($comment_preview) > COMMENT_SNIPPET_LENGTH)
+			{
+				$comment_spillover_link = '<a href="'.$this->href('', $comment['page_tag'], 'show_comments=1').'#comment_'.$comment['id'].'" title="View comment">[&#8230;]</a>';
+				$comment_preview = substr($comment_preview, 0, COMMENT_SNIPPET_LENGTH).$comment_spillover_link;
+			}
+			$commentlink = '<a href="'.$this->href('', $comment['page_tag'], 'show_comments=1').'#comment_'.$comment['id'].'" title="View comment">'.$comment['page_tag'].'</a>';
+			$comment_by = $comment['user'];
+			if (!$this->LoadUser($comment_by))
+			{
+				$comment_by .= ANONYMOUS_COMMENT_AUTHOR;
+			}
+			// print entry
+			echo '&nbsp;&nbsp;&nbsp;'.$commentlink.COMMENT_AUTHOR_DIVIDER.$comment_by.'<blockquote>'.$comment_preview.'</blockquote>'."\n";
 		}
-
-		$timeformatted = date("H:i T", strtotime($comment["time"]));
-
-		$max_preview_length = 122;
-		$comment_preview = str_replace("<br />", "", $comment["comment"]);
-		if (strlen($comment_preview) > $max_preview_length) {
-			$comment_spillover_link = "<a href=\"".$this->href("", $comment["page_tag"], "show_comments=1")."#comment_".$comment["id"]."\" title=\"View comment\">[.... ]</a>";		
-			$comment_preview = substr($comment_preview, 0, $max_preview_length).$comment_spillover_link;
-		}
-		$commentlink = "<a href=\"".$this->href("", $comment["page_tag"], "show_comments=1")."#comment_".$comment["id"]."\" title=\"View comment\">".$comment["page_tag"]."</a>";		
-		
-		$comment_by = $comment["user"];	
-		if (!$this->LoadUser($comment_by)) $comment_by .= " (unregistered user)";
-
-		// print entry
-		print("&nbsp;&nbsp;&nbsp; $commentlink, comment by $comment_by: <br />\n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<em>$comment_preview</em><br />\n");
+	}
+	if ($readable == 0)
+	{
+		echo '<em>'.NO_READABLE_RECENTLY_COMMENTED.'</em>';
 	}
 }
 else
 {
-	print("<em>There are no recently commented pages.</em>");
+	echo '<em>'.NO_RECENTLY_COMMENTED.'</em>';
 }
 
 ?>
