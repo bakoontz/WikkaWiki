0506dbddc19ca354e0a336460f399dcdb2abc6b8
Dario Taraborelli <dartar@wikkawiki.org>
2006-07-18 11:10:19 +0000

    Adding support for Clone ACL option (Credits: BrianKoontz) #189

--- handlers/page/clone.php
+++ handlers/page/clone.php
@@ -7,21 +7,24 @@
  * This handler checks the existence of the source page, the validity of the 
  * name of the target page to be created, the user's read-access to the source 
  * page and write-access to the target page.
- * If the edit option is selected, the user is redirected to the target page for
+ * If the "Edit after creation" option is selected, the user is redirected to the target page for
  * edition immediately after its creation.
+ * If the "Clone ACL" option is selected, ACL settings are copied to the target page, 
+ * otherwise default ACL are applied to the new page.
  *
  * @package         Handlers
  * @subpackage        Page
+ * @version           $Id$
+ * @since             Wikka 1.1.6.0
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
  *
- * @author            {@link http://wikkawiki.org/ChristianBarthelemy Christian Barthelemy} - original idea and code.
- * @author            {@link http://wikkawiki.org/DarTar Dario Taraborelli} - bugs fixed, code improved, removed popup alerts.  
- * @version           $Id$
- * @since             Wikka 1.1.6.0
- *                      
+ * @author		{@link http://wikkawiki.org/ChristianBarthelemy Christian Barthelemy} - original idea and code.
+ * @author		{@link http://wikkawiki.org/DarTar Dario Taraborelli} - bugs fixed, code improved, removed popup alerts.
+ * @author		{@link http://wikkawiki.org/BrianKoontz Brian Koontz} - clone ACL option
+ * 
  * @input             string  $to  required: the page to be created
- *                            must be a non existing page and current user must be authorized to create it
+ *                            must be a non-existing page and current user must have privs to create it
  *                            default is source page name 				
  * 
  * @input             string  $note  optional: the note to be added to the page when created
@@ -29,6 +32,9 @@
  * 
  * @input             boolean $editoption optional: if true, the new page will be opened for edition on creation
  *                            default is false (to allow multiple cloning of the same source)
+ *
+ * @input             boolean $cloneaclsoption optional: if true, ACLs are copied from the source page to the new page
+ *                            default is false
  *
  * @uses			  Wakka::ExistsPage()
  * @uses			  Wakka::Format() 
@@ -52,25 +58,27 @@ if(!defined('VALID_PAGENAME_PATTERN')) define ('VALID_PAGENAME_PATTERN', '/^[A-Z
 /**
  * i18n
  */
-define('CLONE_HEADER', '==== Clone current page ====');
-define('CLONE_SUCCESSFUL', '%s was succesfully created!');
-define('CLONE_X_TO', 'Clone %s to:');
-define('CLONED_FROM', 'Cloned from %s');
-define('EDIT_NOTE', 'Edit note:');
-define('ERROR_ACL_READ', 'You are not allowed to read the source of this page.');
-define('ERROR_ACL_WRITE', 'Sorry! You don\'t have write-access to %s');
-define('ERROR_INVALID_PAGENAME', 'This page name is invalid. Valid page names must start with a letter and contain only letters and numbers.');
-define('ERROR_PAGE_ALREADY_EXIST', 'Sorry, the destination page already exists');
-define('ERROR_PAGE_NOT_EXIST', ' Sorry, page %s does not exist.');
-define('LABEL_CLONE', 'Clone');
-define('LABEL_EDIT_OPTION', ' Edit after creation ');
-define('PLEASE_FILL_VALID_TARGET', 'Please fill in a valid target ""PageName"" and an (optional) edit note.');
+if (!defined('CLONE_HEADER')) define('CLONE_HEADER', '==== Clone current page ====');
+if (!defined('CLONE_SUCCESSFUL')) define('CLONE_SUCCESSFUL', '%s was succesfully created!');
+if (!defined('CLONE_X_TO')) define('CLONE_X_TO', 'Clone %s to:');
+if (!defined('CLONED_FROM')) define('CLONED_FROM', 'Cloned from %s');
+if (!defined('EDIT_NOTE')) define('EDIT_NOTE', 'Edit note:');
+if (!defined('ERROR_ACL_READ')) define('ERROR_ACL_READ', 'You are not allowed to read the source of this page.');
+if (!defined('ERROR_ACL_WRITE')) define('ERROR_ACL_WRITE', 'Sorry! You don\'t have write-access to %s');
+if (!defined('ERROR_INVALID_PAGENAME')) define('ERROR_INVALID_PAGENAME', 'This page name is invalid. Valid page names must start with a letter and contain only letters and numbers.');
+if (!defined('ERROR_PAGE_ALREADY_EXIST')) define('ERROR_PAGE_ALREADY_EXIST', 'Sorry, the destination page already exists');
+if (!defined('ERROR_PAGE_NOT_EXIST')) define('ERROR_PAGE_NOT_EXIST', ' Sorry, page %s does not exist.');
+if (!defined('LABEL_CLONE')) define('LABEL_CLONE', 'Clone');
+if (!defined('LABEL_EDIT_OPTION')) define('LABEL_EDIT_OPTION', ' Edit after creation');
+if (!defined('LABEL_ACL_OPTION')) define('LABEL_ACL_OPTION', ' Clone ACL');
+if (!defined('PLEASE_FILL_VALID_TARGET')) define('PLEASE_FILL_VALID_TARGET', 'Please fill in a valid target ""PageName"" and an (optional) edit note.');
 
 // initialization
 $from = $this->tag;
 $to = $this->tag;
 $note = sprintf(CLONED_FROM, $from);
 $editoption = ''; 
+$cloneaclsoption = '';
 $box = PLEASE_FILL_VALID_TARGET;
 
 echo '<div class="page">'."\n"; //TODO: move to templating class
@@ -99,6 +107,7 @@ if (!$this->ExistsPage($from))
 			$to = isset($_POST['to']) && $_POST['to'] ? $_POST['to'] : $to;
 			$note = isset($_POST['note']) && $_POST['note'] ? $_POST['note'] : $note;
 			$editoption = (isset($_POST['editoption']))? 'checked="checked"' : '';
+			$cloneaclsoption = (isset($_POST['cloneaclsoption']))? 'checked="checked"' : '';
 		
 			// 3. check target pagename validity
 			if (!preg_match(VALID_PAGENAME_PATTERN, $to))  //TODO use central regex library
@@ -121,9 +130,18 @@ if (!$this->ExistsPage($from))
 					} else
 					{
 						// 6. Valid request - proceed to page cloning
-						$thepage=$this->LoadPage($from); # load the source page
-						if ($thepage) $pagecontent = $thepage['body']; # get its content
-						$this->SavePage($to, $pagecontent, $note); #create target page
+						$thepage=$this->LoadPage($from);
+						if ($thepage) $pagecontent = $thepage['body'];
+						$this->SavePage($to, $pagecontent, $note);
+						// Clone ACLs if requested
+						if ($cloneaclsoption == 'checked="checked"')
+						{
+							$acl = $this->LoadAllACLs($from, 0);
+							$this->SaveACL($to, 'read', $this->TrimACLs($acl['read_acl']));
+							$this->SaveACL($to, 'write', $this->TrimACLs($acl['write_acl']));
+							$this->SaveACL($to, 'comment', $this->TrimACLs($acl['comment_acl']));
+						}
+						// Open editor if requested
 						if ($editoption == 'checked="checked"')
 						{
 							// quick edit
@@ -152,6 +170,11 @@ if (!$this->ExistsPage($from))
 			'<td></td>'."\n".
 			'<td>'."\n".
 			'<input type="checkbox" name="editoption" '.$editoption.' id="editoption" /><label for="editoption">'.LABEL_EDIT_OPTION.'</label>'."\n".
+			'<input type="checkbox" name="cloneaclsoption" '.$cloneaclsoption.' id="cloneaclsoption" /><label for="cloneaclsoption">'.LABEL_ACL_OPTION.'</label>'."\n".
+			'</tr>'."\n".
+			'<tr>'."\n".
+			'<td></td>'."\n".
+			'<td>'."\n".
 			'<input type="submit" name="create" value="'.LABEL_CLONE.'" />'."\n".
 			'</td>'."\n".
 			'</tr>'."\n".
