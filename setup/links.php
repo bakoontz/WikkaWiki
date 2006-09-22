b431386adb03a50280ba444c7f6a2f5e0f7a8830
Dario Taraborelli <dartar@wikkawiki.org>
2006-09-22 11:44:11 +0000

    Fixing charset issues, indenting, obsolete formatter regex and phpdoc header. Adding todo's. Please check.

--- setup/links.php
+++ setup/links.php
@@ -1,17 +1,28 @@
 <?php
-	/**
-	 * Re-generating links table.
-	 vim: se encoding=utf-8:
-		* Since 1.1.7.0, we have made an enhacement (optimization) in the categorization process.
-		* Instead of querying all latest page looking for the word CategoryName, we should now do
-		* the search inside the links table, and consider all pages linking to CategoryName belong
-		* to that category. We must then fix an issue from previous versions of Wikka where links
-		* on pages created from the Installer are not reported in the links table. Another issue
-		* which is reported is that older versions of delete handler didn't clean up links table.
-		* @author {@link http://wikkawiki.org/Mokoshi Vincent FrÃ©tin}
-		* @author {@link http://wikkawiki.org/DotMG DotMG}
-		*/
-		error_reporting(E_ALL);
+/**
+ * Re-generate the links table
+ * 
+ * Since 1.1.7, we have made an enhacement (optimization) in the categorization process.
+ * Instead of querying all latest page looking for the word CategoryName, we should now do
+ * the search inside the links table, and consider all pages linking to CategoryName belong
+ * to that category. We must then fix an issue from previous versions of Wikka where links
+ * on pages created from the Installer are not reported in the links table. Another issue
+ * which is reported is that older versions of delete handler didn't clean up links table.
+ * 
+ * @package	Setup
+ * @version $Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ * 
+ * @author {@link http://wikkawiki.org/Mokoshi Vincent Frétin}
+ * @author {@link http://wikkawiki.org/DotMG Mahefa Randimbisoa}
+ * 
+ * @todo use a central RegEx library #34;
+ * @todo apply coding guidelines;
+ * @todo document functions using phpdoc syntax;
+ * @todo refactor main documentation header
+ */
+error_reporting(E_ALL);
 set_time_limit(30);
 // You use less RAM if you do the job step by step, but this increases page generation time
 // This avoid the script from crashing if you have thousands of pages.
@@ -47,18 +58,20 @@ while ($pages = LoadSomePages($start))
 		preg_replace_callback(
 		"/(?>".
 		"%%.*?%%|".																				# code
-		"\"\".*?\"\"|".																			# literal
-		#$mind_map_pattern.
+		"\[\[[^\[]*?\]\]|".																		# forced link
+		#$mind_map_pattern.																		# (safe to be ignored)
 		"\[\[\S*[^\[]*?\]\]|".																		# forced link
-		# safe to be ignored "-{4,}|---|".																			# separator (hr) 
+		#"-{3,}|".																			# forced linebreak and hr (safe to be ignored)
 		"\b[a-z]+:\/\/\S+|".																	# URL
-		# safe to be ignored "\*\*|\'\'|\#\#|\#\%|@@|::c::|\>\>|\<\<|&pound;&pound;|&yen;&yen;|\+\+|__|<|>|\/\/|".	# Wiki markup
-		# safe to be ignored "======|=====|====|===|==|".															# headings
-		# safe to be ignored "\n([\t~]+)(-|&|[0-9a-zA-Z]+\))?|".														# indents and lists
+		#"\*\*|\'\'|\#\#|\#\%|@@|::c::|\>\>|\<\<|&pound;&pound;|&yen;&yen;|\+\+|__|<|>|\/\/|".	# Wiki markup (safe to be ignored)
+		#"======|=====|====|===|==|".															# headings (safe to be ignored)
+		#"\n[\t~]+(-|&|[0-9a-zA-Z]+\))?|".														# indents and lists (safe to be ignored)
+		"\|(?:[^\|])?\|(?:\(.*?\))?(?:\{[^\{\}]*?\})?(?:\n)?|".										# Simple Tables	
 		"\{\{.*?\}\}|".																			# action
-		"\b[A-ZÃ„Ã–Ãœ][A-Za-zÃ„Ã–ÃœÃŸÃ¤Ã¶Ã¼]+[:](?![=_])\S*\b|".											# InterWiki link
-		"\b([A-ZÃ„Ã–Ãœ]+[a-zÃŸÃ¤Ã¶Ã¼]+[A-Z0-9Ã„Ã–Ãœ][A-Za-z0-9Ã„Ã–ÃœÃŸÃ¤Ã¶Ã¼]*)\b".								# CamelWords
-  ")/ms", "relinkcallback", $page['body']);
+		"\b[A-ZÄÖÜ][A-Za-zÄÖÜßäöü]+[:](?![=_])\S*\b|".											# InterWiki link
+		"\b([A-ZÄÖÜ]+[a-zßäöü]+[A-Z0-9ÄÖÜ][A-Za-z0-9ÄÖÜßäöü]*)\b|".								# CamelWords
+		"\n".																					# new line
+  		")/ms", "relinkcallback", $page['body']);
 	}
 	$start = $page['tag'];
 	// insert into wikka_links a batch of records at a time.
@@ -82,11 +95,11 @@ function relinkcallback($thing)
 		return;
 	}
 	// If not on cleanup, try to construct the part ('a', 'b'), ('c', 'd'), ...
-	$thing=$thing[0];
+	$thing = $thing[0];
 	// This regexp treats CamelCases and [[forcedlink]] (or [[http://external.link of any type]])
-	// In case of a Forced link, it treats only a word before the first space or closing bracket THAT satisfies the regexp [A-ZÃ„Ã–Ãœa-zÃŸÃ¤Ã¶Ã¼]+
+	// In case of a Forced link, it treats only a word before the first space or closing bracket THAT satisfies the regexp [A-ZÄÖÜa-zßäöü]+
 	// So, http links like above are not taken into account, because they contain : and /
-if (preg_match("/^(\[\[)?([A-ZÃ„Ã–Ãœa-zÃŸÃ¤Ã¶Ã¼]+(?(1)(?=[ \]])|[A-Z0-9Ã„Ã–Ãœ][A-Za-z0-9Ã„Ã–ÃœÃŸÃ¤Ã¶Ã¼]*\\b$))/s", $thing, $matches))          # recognize forced links across lines
+	if (preg_match("/^(\[\[)?([A-ZÄÖÜa-zßäöü]+(?(1)(?=[ \]])|[A-Z0-9ÄÖÜ][A-Za-z0-9ÄÖÜßäöü]*\\b$))/s", $thing, $matches))          # recognize forced links across lines
 	{
 		$url = $matches[2];
 		if (($url) && (!isset($written[strtolower($url)])) && (strtolower($url) != strtolower($tag)))
