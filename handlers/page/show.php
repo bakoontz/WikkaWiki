d7030561e0b77d43ed65f84041e2af1e9766d06f
Nils Lindenberg <nilslindenberg@wikkawiki.org>
2006-10-03 20:42:33 +0000

    #265: superfluous call of GetUserName(); bug allowing anonymus users to delete comments in all cases; work on i18n

--- handlers/page/show.php
+++ handlers/page/show.php
@@ -31,6 +31,21 @@
  * @todo		move <div> to template;
  * @todo		i18n;
  */
+
+/**
+ * i18n 
+ */
+if (!defined('ERROR_NO_ACCESS')) define('ERROR_NO_ACCESS', "You aren't allowed to read this page.");
+if (!defined('COMMENTS_HEADER')) define('COMMENTS_HEADER', 'Comments');
+if (!defined('HIDE_COMMENTS')) define('HIDE_COMMENTS', 'Hide comments/form');
+if (!defined('ADD_COMMENT_LABEL')) define('ADD_COMMENT_LABEL', 'Add a comment to this page:');
+if (!defined('ADD_COMMENT')) define('ADD_COMMENT', 'Add comment');
+if (!defined('BUTTON_ADD_COMMENT')) define('BUTTON_ADD_COMMENT', 'Add Comment');
+if (!defined('DISPLAY_COMMENT')) define('DISPLAY_COMMENT', 'Display comment');
+if (!defined('DISPLAY_COMMENTS')) define('DISPLAY_COMMENTS', 'Display comments');
+if (!defined('NO_COMMENTS')) define('NO_COMMENTS', 'There are no comments on this page.');
+if (!defined('ONE_COMMENT')) define('ONE_COMMENT', 'There is one comment on this page.');
+if (!defined('SOME_COMMENTS')) define('SOME_COMMENTS', 'There are %d comments on this page. ');
  
 echo '<div class="page"';
 echo (($user = $this->GetUser()) && ($user['doubleclickedit'] == 'N') || !$this->HasAccess('write')) ? '' : 'ondblclick="document.location=\''.$this->Href('edit').'\';" '; #268
@@ -38,7 +53,7 @@ echo '>'."\n";//TODO: move to templating class
 
 if (!$this->HasAccess('read'))
 {
-	echo '<p><em class="error">You aren\'t allowed to read this page.</em></p></div>'; #i18n
+	echo '<p><em class="error">'.ERROR_NO_ACCESS.'</em></p></div>';
 }
 else
 {
@@ -50,7 +65,7 @@ else
 	{
 		if ($this->page['latest'] == 'N')
 		{
-			echo '<div class="revisioninfo">This is an old revision of <a href="'.$this->Href().'">'.$this->GetPageTag().'</a> from '.$this->page['time'].'.</div>';
+			echo '<div class="revisioninfo">This is an old revision of <a href="'.$this->Href().'">'.$this->GetPageTag().'</a> from '.$this->page['time'].'.</div>'; #i18n
 		}
 
 		// display page
@@ -63,7 +78,7 @@ else
 			if ($latest = $this->LoadPage($this->tag))
 			{
 ?>
-		            <br />
+		        <br />
  				<?php echo $this->FormOpen('edit') ?>
  				<input type="hidden" name="previous" value="<?php echo $latest['id'] ?>" />
  				<input type="hidden" name="body" value="<?php echo $this->htmlspecialchars_ent($this->page['body']) ?>" />
@@ -102,13 +117,14 @@ else
 				// display comments header
 ?>
 				<div class="commentsheader">
-				<span id="comments">&nbsp;</span>Comments [<a href="<?php echo $this->Href('', '', 'show_comments=0') ?>">Hide comments/form</a>]
+				<span id="comments">&nbsp;</span><?php echo COMMENTS_HEADER; ?> [<a href="<?php echo $this->Href('', '', 'show_comments=0') ?>"><?php echo HIDE_COMMENTS; ?></a>]
 				</div>
 <?php
 				// display comments themselves
 				if ($comments)
 				{
 					$current_user = $this->GetUserName(); 
+					$is_owner = $this->UserIsOwner();
 		 			foreach ($comments as $comment)
 					{
 						echo '<div class="comment">'."\n".
@@ -116,8 +132,7 @@ else
 							"\t".'<div class="commentinfo">'."\n-- ";
 						echo ($this->LoadUser($comment['user']))? $this->Format($comment['user']) : $comment['user']; // #84
 						echo ' ('.$comment['time'].')'."\n";
-						$current_user = $this->GetUserName(); 
-     						if ($this->UserIsOwner() || $current_user == $comment['user'] || ($this->config['anony_delete_own_comments'] && $current_user == $comment['user']) )
+   						if ($is_owner || $user['name'] == $comment['user'] || ($this->config['anony_delete_own_comments'] && $current_user == $comment['user']))
 						{
 							echo $this->FormOpen("delcomment");
 ?>
@@ -135,9 +150,9 @@ else
 				if ($this->HasAccess('comment'))
 				{?>
 		    			<?php echo $this->FormOpen('addcomment'); ?>
-					<label for="commentbox">Add a comment to this page:<br />
+					<label for="commentbox"><?php echo ADD_COMMENT_LABEL; ?><br />
 					<textarea id="commentbox" name="body" rows="6" cols="78"></textarea><br />
-					<input type="submit" value="Add Comment" accesskey="s" />
+					<input type="submit" value="<?php echo BUTTON_ADD_COMMENT; ?>" accesskey="s" />
             			</label>
 					<?php echo $this->FormClose(); ?>
 				<?php
@@ -150,21 +165,20 @@ else
 				switch (count($comments))
 				{
 				case 0:
-					$comments_message = 'There are no comments on this page. '; #i8n
-					$showcomments_text = 'Add comment'; #i8n
+					$comments_message = NO_COMMENTS.' ';
+					$showcomments_text = ADD_COMMENT;
 					$comment_form_link  = ($this->HasAccess('comment')) ? 1 : 0;
 					break;
 				case 1:
-					$comments_message = 'There is one comment on this page. '; #i8n
-					$showcomments_text = 'Display comment'; #i8n
+					$comments_message = ONE_COMMENT.' ';
+					$showcomments_text = DISPLAY_COMMENT;
 					$comment_form_link = 1;
 					break;
 				default:
-					$comments_message = 'There are '.count($comments).' comments on this page. '; #i8n
-					$showcomments_text = 'Display comments'; #i8n
+					$comments_message = sprintf(SOME_COMMENTS, count($comments));
+					$showcomments_text = DISPLAY_COMMENTS;
 					$comment_form_link = 1;
 				}
-
 
 				echo $comments_message;
 				if ($comment_form_link == 1)
