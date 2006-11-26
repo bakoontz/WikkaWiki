355d373ec7ccf5b690649a71f672412ec5e505e1
Nils Lindenberg <nilslindenberg@wikkawiki.org>
2006-11-26 19:49:30 +0000

    #377: rewrite of mychanges.php

--- actions/mychanges.php
+++ actions/mychanges.php
@@ -11,100 +11,100 @@
  * @filesource
  * 
  * @author Carlo Zottmann
+ * @author	{@link http://wikkawiki.org/NilsLindenberg Nils Lindenberg} (rewrite, i18n)
  * 
  * @uses	Wakka::GetUser()
+ * @uses	Wakka::GetUserName()
+ * @uses	Wakka::GetPageTag()
+ * @uses	Wakka::href()
  * @uses	Wakka::LoadAll()
  * @uses	Wakka::Link()
- * @uses	Wakka::href()
- * @uses	Wakka::GetUserName()
  */
+
+/**
+ * i18n
+ */
+if (!defined('NO_PAGES_FOUND')) define ('NO_PAGES_FOUND', 'No pages found.');
+if (!defined('NO_PAGES_EDITED')) define ('NO_PAGES_EDITED', 'You have not edited any pages yet.');
+if (!defined('ALPHA_PAGES_CHANGE_LIST')) define ('ALPHA_PAGES_CHANGE_LIST', "This is a list of pages you've edited, along with the time of your last change.");
+if (!defined('TIME_PAGES_CHANGE_LIST')) define ('TIME_PAGES_CHANGE_LIST', "This is a list of pages you've edited, ordered by the time of your last change.");
+if (!defined('ORDER_DATE')) define ('ORDER_DATE', 'order by date');
+if (!defined('ORDER_ALPHA')) define ('ORDER_ALPHA', 'order alphabetically');
+if (!defined('NOT_LOGGED_IN')) define ('NOT_LOGGED_IN', "You're not logged in, thus the list of pages you've edited couldn't be retrieved.");
+
+// order alphabetically or after time?
+$alpha = FALSE;
+if (isset($_GET["alphabetically"]) && $_GET["alphabetically"] == 1) $alpha = TRUE;
+
+$tag = $this->GetPageTag();
+$output = '';
 
 if ($user = $this->GetUser())
 {
 	$my_edits_count = 0;
+	
+	// header
+	$output .= '<strong>';
+	if ($alpha) $output .= ALPHA_PAGES_CHANGE_LIST.' (<a href="'.$this->href("", $tag).'">'.ORDER_DATE;
+ 	else $output .= TIME_PAGES_CHANGE_LIST.' (<a href="'.$this->href("", $tag, "alphabetically=1").'">'.ORDER_ALPHA;
+	$output .= '</a>)</strong><br /><br />'."\n";
 
-	if ($_REQUEST["alphabetically"] == 1)
+	// get the pages
+	$query = "SELECT tag, time FROM ".$this->config["table_prefix"]."pages WHERE user = '".mysql_real_escape_string($this->GetUserName())."' AND latest = 'Y' ";
+	if ($alpha) $query .= "ORDER BY tag ASC, time DESC";
+	else $query .= "ORDER BY time DESC, tag ASC";
+	
+	if ($pages = $this->LoadAll($query))
 	{
-		print("<strong>This is a list of pages you've edited, along with the time of your last change (<a href=\"".$this->href("", $tag)."\">order by date</a>).</strong><br /><br />\n");	#i18n
-
-		if ($pages = $this->LoadAll("SELECT tag, time FROM ".$this->config["table_prefix"]."pages WHERE user = '".mysql_real_escape_string($this->GetUserName())."' ORDER BY tag ASC, time DESC"))
-		{
-			foreach ($pages as $page)
-			{
-				if ($last_tag != $page["tag"]) {
-					$last_tag = $page["tag"];
-					$firstChar = strtoupper($page["tag"][0]);
-					if (!preg_match("/[A-Z,a-z]/", $firstChar)) {
-						$firstChar = "#";
-					}
+		$current = '';
 		
-					if ($firstChar != $curChar) {
-						if ($curChar) print("<br />\n");
-						print("<strong>$firstChar</strong><br />\n");
-						$curChar = $firstChar;
-					}
-	
-					// print entry
-					print("&nbsp;&nbsp;&nbsp;(".$page["time"].") (".$this->Link($page["tag"], "revisions", "history", 0).") ".$this->Link($page["tag"], "", "", 0)."<br />\n");
-	
-					$my_edits_count++;
+		// build the list of pages
+		foreach ($pages as $page) 
+		{
+			// order alphabetically
+			if($alpha)
+			{
+				$firstChar = strtoupper($page["tag"][0]);
+				if (!preg_match("/[A-Z,a-z]/", $firstChar)) $firstChar = "#";
+		
+				if ($firstChar != $current) 
+				{
+					if ($current) $output .= "<br />\n";
+					$output .= '<strong>'.$firstChar."</strong><br />\n";
+					$current = $firstChar;
 				}
 			}
-			
-			if ($my_edits_count == 0)
+			// order after time
+			else
 			{
-				print("<em>You have not edited any pages yet.</em>"); #i18n
+				// day header
+				list($day, $time) = explode(" ", $page["time"]);		
+				if ($day != $current)
+				{
+					if ($current) print("<br />\n");
+					$output .= '<strong>'.$day.':</strong><br />'."\n";
+					$current = $day;
+				}
 			}
+			$output .= "&nbsp;&nbsp;&nbsp;(".$page["time"].") (".$this->Link($page["tag"], "revisions", "history", 0).") ".$this->Link($page["tag"], "", "", 0)."<br />\n";
+			$my_edits_count++;
 		}
-		else
+		
+		if ($my_edits_count == 0)
 		{
-			print("<em>No pages found.</em>"); #i18n
+			$output .= '<em>'.NO_PAGES_EDITED.'</em>';
 		}
 	}
 	else
 	{
-		print("<strong>This is a list of pages you've edited, ordered by the time of your last change (<a href=\"".$this->href("", $tag, "alphabetically=1")."\">order alphabetically</a>).</strong><br /><br />\n"); #i18n	
-
-		if ($pages = $this->LoadAll("SELECT tag, time FROM ".$this->config["table_prefix"]."pages WHERE user = '".mysql_real_escape_string($this->GetUserName())."' ORDER BY time ASC, tag ASC"))
-		{
-			foreach ($pages as $page)
-			{
-				$edited_pages[$page["tag"]] = $page["time"];
-			}
-
-			$edited_pages = array_reverse($edited_pages);
-
-			foreach ($edited_pages as $page["tag"] => $page["time"])
-			{
-				// day header
-				list($day, $time) = explode(" ", $page["time"]);
-				if ($day != $curday)
-				{
-					if ($curday) print("<br />\n");
-					print("<strong>$day:</strong><br />\n");
-					$curday = $day;
-				}
-
-				// print entry
-				print("&nbsp;&nbsp;&nbsp;($time) (".$this->Link($page["tag"], "revisions", "history", 0).") ".$this->Link($page["tag"], "", "", 0)."<br />\n");
-
-				$my_edits_count++;
-			}
-			
-			if ($my_edits_count == 0)
-			{
-				print("<em>You have not edited any pages yet.</em>"); #i18n
-			}
-		}
-		else
-		{
-			print("<em>No pages found.</em>"); #i18n
-		}
+		$output .= '<em>'.NO_PAGES_FOUND.'</em>';
 	}
 }
 else
 {
-	print("<em>You're not logged in, thus the list of pages you've edited couldn't be retrieved.</em>"); #i18n
+	$output .= '<em>'.NOT_LOGGED_IN.'</em>';
 }
 
+// *** output section ***
+print $output;
 ?>
