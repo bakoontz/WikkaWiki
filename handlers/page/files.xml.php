c3167c14628f7c53d490b76ecd8739c5185e795c
Mahefa Randimbisoa <dotmg@wikkawiki.org>
2006-12-30 15:38:05 +0000

    #382 : +Disabling output buffering for files.xml

--- handlers/page/files.xml.php
+++ handlers/page/files.xml.php
@@ -14,6 +14,7 @@
  * @uses		Wakka::Href()
  * @uses		Wakka::IsAdmin()
  * @uses		Wakka::redirect()
+	* @todo WikkaCodingGuidelinesHowTo
  */
 
 // upload path
@@ -24,15 +25,37 @@ if (! is_dir($upload_path)) mkdir_r($upload_path);
 // do the action
 switch ($_REQUEST['action']) {
     case 'download':
+												header('Accept-Ranges: bytes');
             $_REQUEST['file'] = basename($_REQUEST['file']);
-            if ($this->HasAccess('read')) {
-                $path = "{$upload_path}/{$_REQUEST['file']}";
+            $path = "{$upload_path}/{$_REQUEST['file']}";
                 $filename = basename($path);
-		    Header("Content-Length: ".filesize($path));
 		    Header("Content-Type: application/x-download");
 		    Header("Content-Disposition: attachment; filename=\"".urldecode($filename)."\"");
-    		    Header("Connection: close");
-    		    @readfile($path);
+            if ($this->HasAccess('read')) {
+																				if (isset($_SERVER['HTTP_RANGE']) && (preg_match('/^.*bytes[= ]+(\d+)-(\d+)\s*$/', $_SERVER['HTTP_RANGE'], $range)) && (intval($range[2]) >= intval($range[1])))
+																				{
+																												$rstart = $range[1];
+																												$rend = $range[2];
+																												$fp = fopen($path, 'rb');
+																												fseek($fp, $rstart+SEEK_SET);
+																												$data = fread($fp, $rend - $rstart + 1);
+																												fclose($fp);
+																												header('Content-Range: bytes '.$rstart.'-'.$rend.'/'.filesize($path));
+																												header('HTTP/1.1 206 Partial content');
+																												echo $data;
+																												exit();
+																				}
+		    //Header("Content-Length: ".filesize($path));
+    		    //Header("Connection: close");
+										ob_end_clean();
+										ob_end_clean();
+										$fp = fopen($path, 'rb');
+										while ($data = fread($fp, 4096))
+										{
+																		echo $data;
+										}
+										fclose($fp);
+    		    //@readfile($path);
 		    exit();
             }
     case 'delete':   
