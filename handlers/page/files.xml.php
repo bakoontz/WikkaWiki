84a3d404127b8c590916308a667cea8592411a90
Mahefa Randimbisoa <dotmg@wikkawiki.org>
2006-12-31 11:15:31 +0000

    #244: phpDoc & WikkaCodingGuidelinesHowTo. No other potential change in this changeset.

--- handlers/page/files.xml.php
+++ handlers/page/files.xml.php
@@ -1,7 +1,15 @@
 <?php
 /**
  * Handle download/deletion of a file. 
+ *
+ * Only files uploaded using the Wiki can be downloaded/deleted using this handler, 
+ * and every user who has read access to the page to which the files are attached
+ * can download them. For the deletion, only administrators can delete files.
+ * Range: and Accept-Range: headers are supported, so advanced downloader tools can
+ * be used to download heavy size files.
  * 
+ * See also {@link files.php}, {@link Config::$upload_path}.
+ *
  * @package		Handlers
  * @subpackage	Files
  * @version		$Id$
@@ -14,7 +22,7 @@
  * @uses		Wakka::Href()
  * @uses		Wakka::IsAdmin()
  * @uses		Wakka::redirect()
-	* @todo WikkaCodingGuidelinesHowTo
+ * @uses		Config::$upload_path
  */
 
 // upload path
@@ -23,47 +31,48 @@ $upload_path = $this->config['upload_path'].'/'.$this->GetPageTag();
 if (! is_dir($upload_path)) mkdir_r($upload_path);
 
 // do the action
-switch ($_REQUEST['action']) {
-    case 'download':
-												header('Accept-Ranges: bytes');
-            $_REQUEST['file'] = basename($_REQUEST['file']);
-            $path = "{$upload_path}/{$_REQUEST['file']}";
-                $filename = basename($path);
-		    Header("Content-Type: application/x-download");
-		    Header("Content-Disposition: attachment; filename=\"".urldecode($filename)."\"");
-            if ($this->HasAccess('read')) {
-																				if (isset($_SERVER['HTTP_RANGE']) && (preg_match('/^.*bytes[= ]+(\d+)-(\d+)\s*$/', $_SERVER['HTTP_RANGE'], $range)) && (intval($range[2]) >= intval($range[1])))
-																				{
-																												$rstart = $range[1];
-																												$rend = $range[2];
-																												$fp = fopen($path, 'rb');
-																												fseek($fp, $rstart+SEEK_SET);
-																												$data = fread($fp, $rend - $rstart + 1);
-																												fclose($fp);
-																												header('Content-Range: bytes '.$rstart.'-'.$rend.'/'.filesize($path));
-																												header('HTTP/1.1 206 Partial content');
-																												echo $data;
-																												exit();
-																				}
-		    //Header("Content-Length: ".filesize($path));
-    		    //Header("Connection: close");
-										@ob_end_clean();
-										@ob_end_clean();
-										$fp = fopen($path, 'rb');
-										while (!feof($fp))
-										{
-																		$data = fread($fp, 4096);
-																		echo $data;
-										}
-										fclose($fp);
-    		    //@readfile($path);
-		    exit();
-            }
-    case 'delete':   
-            // if ($this->HasAccess('write')) {
-		if ($this->IsAdmin()) {
-                @unlink("{$upload_path}/{$_REQUEST['file']}");
-            }
-            print $this->redirect($this->Href());
+switch ($_REQUEST['action']) 
+{
+	case 'download':
+		header('Accept-Ranges: bytes');
+		$_REQUEST['file'] = basename($_REQUEST['file']);
+		$path = "{$upload_path}/{$_REQUEST['file']}";
+		$filename = basename($path);
+		Header("Content-Type: application/x-download");
+		Header("Content-Disposition: attachment; filename=\"".urldecode($filename)."\"");
+		if ($this->HasAccess('read')) 
+		{
+			if (isset($_SERVER['HTTP_RANGE']) && (preg_match('/^.*bytes[= ]+(\d+)-(\d+)\s*$/', $_SERVER['HTTP_RANGE'], $range)) && (intval($range[2]) >= intval($range[1])))
+			{
+				$rstart = $range[1];
+				$rend = $range[2];
+				$fp = fopen($path, 'rb');
+				fseek($fp, $rstart+SEEK_SET);
+				$data = fread($fp, $rend - $rstart + 1);
+				fclose($fp);
+				header('Content-Range: bytes '.$rstart.'-'.$rend.'/'.filesize($path));
+				header('HTTP/1.1 206 Partial content');
+				echo $data;
+				exit();
+			}
+			//Header("Content-Length: ".filesize($path));
+			//Header("Connection: close");
+			@ob_end_clean();
+			@ob_end_clean();
+			$fp = fopen($path, 'rb');
+			while (!feof($fp))
+			{
+				$data = fread($fp, 4096);
+				echo $data;
+			}
+			fclose($fp);
+			exit();
+		}
+	case 'delete':
+		if ($this->IsAdmin()) 
+		{
+			@unlink("{$upload_path}/{$_REQUEST['file']}");
+		}
+		print $this->redirect($this->Href());
 }
 ?>
