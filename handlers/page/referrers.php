f7060406c2442f5d02f01ffc0c624f1ddd14b74f
Marjolein Katsma <javawoman@wikkawiki.org>
2007-03-05 12:32:40 +0000

    1.1.6.3: applies [294]:[330] diff from trunk; some conflicts resolved

--- handlers/page/referrers.php
+++ handlers/page/referrers.php
@@ -1,18 +1,52 @@
-<div class="page">
 <?php
+/**
+ * Show external referrers linking to the current page or to this wiki.
+ * 
+ * @package		Handlers
+ * @subpackage	Referrers
+ * @version		$Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ * 
+ * @uses  Config::$referrers_purge_time
+ * @uses		Wakka::GetConfigValue()
+ * @uses		Wakka::GetPageTag()
+ * @uses		Wakka::GetUser()
+ * @uses		Wakka::GetMethod()
+ * @uses		Wakka::Href()
+ * @uses		Wakka::htmlspecialchars_ent()
+ * @uses		Wakka::IsAdmin()
+ * @uses		Wakka::Link
+ * @uses		Wakka::LoadReferrers()
+ * 
+ * @todo		move main <div> to templating class
+ * @todo		better separation between data gathering and output
+ * @todo		$heading should become heading, not bold text
+ */
 
+$global = '';
 $IsAdmin = $this->IsAdmin();
-if ($global = $_REQUEST["global"])
+
+#if ($global = $_REQUEST["global"])
+if (isset($_GET["global"])) #312
 {
+	// referrers to this wiki
+	// prepare UI
+	#$global = $_REQUEST["global"];
+	$global = $_GET["global"]; #312
 	$title = "Sites linking to this wiki (<a href=\"".$this->Href("referrers_sites", "", "global=1")."\">see list of domains</a>):";
 	$referrers = $this->LoadReferrers();
 }
 else
 {
+	// referrers to this page
+	// prepare UI
 	$title = "External pages linking to ".$this->Link($this->GetPageTag()).
 		($this->GetConfigValue("referrers_purge_time") ? " (last ".($this->GetConfigValue("referrers_purge_time") == 1 ? "24 hours" : $this->GetConfigValue("referrers_purge_time")." days").")" : "")." (<a href=\"".$this->Href("referrers_sites")."\">see list of domains</a>):";
 	$referrers = $this->LoadReferrers($this->GetPageTag());
 }
+
+echo '<div class="page">'."\n"; //TODO: move to templating class
 
 print("<strong>$title</strong><br />\n");
 print("<em>Note to spammers: This page is not indexed by search engines, so don't waste your time.</em><br /><br />");
@@ -47,6 +81,5 @@ else
 	print("<br />[<a href=\"".$this->href("referrers_sites", "", "global=1")."\">View global referring sites</a> | <a href=\"".$this->href("referrers", "", "global=1")."\">View global referrers</a> | <a href=\"".$this->href("review_blacklist")."\">View referrer blacklist</a>]");
 }
 
-
+echo '</div>'."\n" //TODO: move to templating class
 ?>
-</div>
