357592b2d487e7936127893448a579dd1dceb0e9
Marjolein Katsma <javawoman@wikkawiki.org>
2007-03-05 07:06:50 +0000

    1.1.6.3: attempt to ''sensibly'' applying [324]:[330] diff from trunk failed, since the file had totally changed. resorted to manual update with the same intent:   - added docblock   - changed $_REQUEST to $_POST (#312)   - applied htmlspecialchars_ent() to user-specified download link description (not this ticket but certainly a security matter)

--- actions/files.php
+++ actions/files.php
@@ -1,4 +1,43 @@
 <?php
+/**
+ * Display a form with file attachments to the current page.
+ * 
+ * This actions displays a form allowing users to download files uploaded to wiki pages. By default only 
+ * wiki admins can upload and delete files. If the intranet mode option is enabled, any user with write access
+ * to the current page can upload or remove file attachments. If the optional download parameter is set, a simple 
+ * download link is displayed for the specified file.
+ *
+ * Usage: {{files [download="filename"] [text="download link text"]}}
+ *
+ * @package		Actions
+ * @version		$Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ * 
+ * @author Victor Manuel Varela (original code)
+ * @author {@link http://wikkawiki.org/CryDust CryDust} (code overhaul, stylesheet)
+ * 
+ * @input 	string 	$download  	optional: prints a link to the file specified in the string
+ * 			string 	$text		optional: a text for the link provided with the download parameter
+ * @output	a form for file uploading/downloading and a table with an overview of attached files
+ *
+ * @uses	Wakka::HasAccess()
+ * @uses	Wakka::IsAdmin()
+ * @uses	Wakka::MiniHref()
+ * @uses	Wakka::htmlspecialchars_ent()
+ *
+ * @todo security: check file type, not only extension
+ * @todo use buttons instead of links for file deletion; #72 comment 7
+ * @todo similarly replace download link with POST form button -> files handler 
+ * 		 can then use only $_POST instead of $_GET
+ * @todo replace intranet mode with fine-grained file ownership/ACL;
+ * @todo integrate with edit handler for easy insertion of file links;
+ * @todo maybe move some internal utilities to Wakka class?
+ * @todo make datetime format configurable;
+ * @todo add support for file versioning;
+ * @todo add (AJAX-powered?) confirmation check on file deletion;
+ * @todo integrate file table in page template, à la Wacko;
+ */
 
 // $max_upload_size = "1048576"; // 1 Megabyte
 $max_upload_size = "2097152"; // 2 Megabyte
@@ -66,6 +105,8 @@ if ($download <> '') {
 
     // link to download a file
     if ($text == '') $text = $download;
+	//Although $output is passed to ReturnSafeHTML, it's better to sanitize $text here. At least it can avoid invalid XHTML.
+	$text = $this->htmlspecialchars_ent($text);
     echo "<a href=\"".$this->href('files.xml',$this->GetPageTag(),'action=download&amp;file='.urlencode($download))."\">".$text."</a>";
 
 // } elseif ($this->page AND $this->HasAccess('write') AND ($this->method <> 'print.xml') AND ($this->method <> 'edit')) {
@@ -81,7 +122,7 @@ if ($download <> '') {
     $uploaded = $_FILES['file'];
    // if ($_SERVER['REQUEST_METHOD'] == 'POST') 
    // if ($_REQUEST['action'] == 'upload' AND $uploaded['size'] > 0) {
-   if ($_REQUEST['action'] == 'upload') {
+   if ($_POST['action'] == 'upload') {
  
 		switch($_FILES['file']['error'])
 		{
@@ -236,4 +277,4 @@ if ($download <> '') {
    print " </table>  ";
 
 }
-?> 
+?>
