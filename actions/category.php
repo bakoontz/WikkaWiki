ffdf8eb3f217ec365ddde0915b5f661114605d34
Marjolein Katsma <javawoman@wikkawiki.org>
2007-03-05 06:59:22 +0000

    1.1.6.3: applying [273]:[330] diff from trunk (#312); some '''manual''' conflict resolution needed

--- actions/category.php
+++ actions/category.php
@@ -1,10 +1,31 @@
 <?php
 if ($cattag = $_REQUEST["wakka"])
+/**
+ * Shows the pages and subcategories belonging to a category.
+ * 
+ * See WikiCategory to understand how the system works.
+ * 
+ * @package		Actions
+ * @version 	$Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ * 
+ * @input	string $page optional: the category for which you want to display the pages and categories. Default: current page
+ * @input	integer $compact optional: produces a columnar layout with a layout table; 1 produces output in the form of an unordered list. Default: 0
+ * @input	integer $col optional: number of columns (for compact=0). Default: 1
+ * @output	A html table with pages
+ * @uses	Wakka::GetPageTag();
+ * @uses	Wakka::FullCategoryTextSearch()
+ * @uses	Wakka::FullTextSearch()
+ */
+ 
+#if ($cattag = $_REQUEST["wakka"])
+if ($cattag = $_GET['wakka'])	#312 (only files action uses POST for wakka)
 {
 	$str ="";
 	if (!isset($col)) $col=1;
 	if (!isset($compact)) $compact=0;
-	if (!isset($page)) $page=$this->getPageTag(); 
+	if (!isset($page)) $page=$this->GetPageTag(); 
 	if ($page=="/") $page="CategoryCategory"; 
 
 //	$page= preg_replace( "/(\w+)\s(\w+)/", "$1$2",$page);
