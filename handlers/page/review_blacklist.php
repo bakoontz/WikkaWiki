b58a55283f1dcf2717ddb5e9cc921400f63030f7
Marjolein Katsma <javawoman@wikkawiki.org>
2007-03-05 12:33:24 +0000

    1.1.6.3: applies [294]:[330] diff from trunk; some conflicts resolved

--- handlers/page/review_blacklist.php
+++ handlers/page/review_blacklist.php
@@ -1,9 +1,33 @@
-<div class="page">
 <?php
+/**
+ * Show a list of blacklisted referrers.
+ * 
+ * Admins have the possibility to remove entries.
+ * 
+ * @package		Handlers
+ * @subpackage	Referrers
+ * @version		$Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ * 
+ * @uses		Wakka::IsAdmin()
+ * @uses		Wakka::Href()
+ * @uses		Wakka::htmlspecialchars_ent()
+ * @uses		Wakka::LoadAll()
+ * @uses		Wakka::Query()
+ * @uses		Wakka::redirect()
+ * 
+ * @todo		move main <div> to templating class
+ * @todo		make valid XHTML (can't mix table with ul)
+ */
 
 $IsAdmin = $this->IsAdmin();
-if ($IsAdmin && ($whitelist = $_REQUEST["whitelist"]))
+
+#if ($IsAdmin && isset($_REQUEST["whitelist"]))
+if ($IsAdmin && isset($_GET["whitelist"]))
 {
+	#$whitelist = $_REQUEST["whitelist"];
+	$whitelist = $_GET["whitelist"];
 	$this->Query("DELETE FROM ".$this->config["table_prefix"]."referrer_blacklist WHERE spammer = '".mysql_real_escape_string($whitelist)."'");
 	$this->redirect($this->Href("review_blacklist"));
 }
@@ -31,5 +55,5 @@ else
 
 print("<br />[<a href=\"".$this->Href("referrers_sites", "", "global=1")."\">View global referring sites</a> | <a href=\"".$this->Href("referrers", "", "global=1")."\">View global referrers</a>]");
 
+echo '</div>'."\n" //TODO: move to templating class
 ?>
-</div>
