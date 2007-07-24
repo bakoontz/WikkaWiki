487123c034be0cf9e0b1c4727c8fc001c7b62213
Marjolein Katsma <javawoman@wikkawiki.org>
2007-07-24 16:34:44 +0000

    - cleanup whitespace - changing references to #312 (done) - removing references to "template"/class

--- handlers/review_blacklist/review_blacklist.php
+++ handlers/review_blacklist/review_blacklist.php
@@ -1,38 +1,33 @@
 <?php
 /**
  * Show a list of blacklisted referrers.
- * 
+ *
  * Admins have the possibility to remove entries.
- * 
+ *
  * @package		Handlers
  * @subpackage	Referrers
  * @version		$Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
- * 
+ *
  * @uses		Wakka::IsAdmin()
  * @uses		Wakka::Href()
  * @uses		Wakka::htmlspecialchars_ent()
  * @uses		Wakka::LoadAll()
  * @uses		Wakka::Query()
  * @uses		Wakka::redirect()
- * 
- * @todo		move main <div> to templating class
+ *
  * @todo		make valid XHTML (can't mix table with ul)
- * @todo	replace $_REQUEST with either $_GET or $_POST (or both if really
- * 			necessary) - #312  
  * @todo		replace with new handlers - #1
  */
 
 $IsAdmin = $this->IsAdmin();
 
-#if ($IsAdmin && isset($_REQUEST["whitelist"]))
-if ($IsAdmin && isset($_GET["whitelist"]))
+if ($IsAdmin && isset($_GET["whitelist"]))	# 312
 {
-	#$whitelist = $_REQUEST["whitelist"];
-	$whitelist = $_GET['whitelist'];
+	$whitelist = $_GET['whitelist'];	# 312
 	$this->Query('DELETE FROM '.$this->config['table_prefix'].'referrer_blacklist WHERE spammer = "'.mysql_real_escape_string($whitelist).'"');
-	$this->Redirect($this->Href("review_blacklist"));
+	$this->Redirect($this->Href('review_blacklist'));
 }
 
 // set up output variables
@@ -45,7 +40,7 @@ $menu = '['.$ref_domains_to_wiki_link.' | '.$ref_urls_to_wiki_link.']';
 // get data
 $blacklist = $this->LoadAll("SELECT * FROM ".$this->config["table_prefix"]."referrer_blacklist");
 
-echo '<div class="page">'."\n"; //TODO: move to templating class
+echo '<div class="page">'."\n";
 echo '<strong>'.BLACKLIST_HEADING.'</strong><br /><br />'."\n";
 
 // present data
