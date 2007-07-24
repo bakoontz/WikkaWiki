3fb447c8d3ba5b9bc02e0656df9417fbf506d4b9
Marjolein Katsma <javawoman@wikkawiki.org>
2007-07-24 15:47:02 +0000

    more direct way to use user name since GetUser() is already used to check whether the user is logged in - no need to repeat that in GetUsername()!

--- handlers/claim/claim.php
+++ handlers/claim/claim.php
@@ -1,28 +1,29 @@
 <?php
 /**
  * Claim the ownership of a page if has no owner, and if user is logged in.
- * 
+ *
  * @package		Handlers
- * @subpackage	Page 
+ * @subpackage	Page
  * @version		$Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
- * 
+ *
  * @uses	Wakka::GetPageOwner()
  * @uses	Wakka::GetPageTag()
  * @uses	Wakka::GetUser()
- * @uses	Wakka::GetUserName()
  * @uses	Wakka::Href()
  * @uses	Wakka::SetRedirectMessager()
  * @uses	Wakka::Redirect()
  */
 
-if ($this->page && !$this->GetPageOwner() && $this->GetUser())
+// only claim ownership if this page has no owner, and if user is logged in.
+#if ($this->page && !$this->GetPageOwner() && $this->GetUser())
+if ($this->page && !$this->GetPageOwner() && ($user = $this->GetUser()))
 {
-	$this->SetPageOwner($this->GetPageTag(), $this->GetUserName());
+	#$this->SetPageOwner($this->GetPageTag(), $this->GetUserName());
+	$this->SetPageOwner($this->GetPageTag(), $user['name']);
 	$this->SetRedirectMessage(USER_IS_NOW_OWNER);
 }
 
 $this->Redirect($this->Href());
-
 ?>
