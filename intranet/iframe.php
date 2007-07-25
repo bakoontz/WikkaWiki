ff6445a84bd994c003d0371abed6879d469eb74d
Marjolein Katsma <javawoman@wikkawiki.org>
2007-07-25 10:48:31 +0000

    - cleanup (whitespace)

--- intranet/iframe.php
+++ intranet/iframe.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Include an (external) page into the current page.
- *  
+ *
  * @package		Actions
  * @version		$Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
- * 
+ *
  * @uses	Wakka::cleanUrl()
  * @uses	Wakka::htmlspecialchars_ent()
  */
- 
+
 $width = $this->htmlspecialchars_ent(trim($vars['width']));
 $height = $this->htmlspecialchars_ent(trim($vars['height']));
 $url = $this->cleanUrl(trim($vars['url']));
