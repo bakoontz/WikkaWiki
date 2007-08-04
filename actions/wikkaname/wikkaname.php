b0d2d9175a7f9f42a401e22569598e6ea7455e2a
Marjolein Katsma <javawoman@wikkawiki.org>
2007-08-04 16:38:47 +0000

    docblock updated refs #496

--- actions/wikkaname/wikkaname.php
+++ actions/wikkaname/wikkaname.php
@@ -3,11 +3,12 @@
  * Print the name of this WikkaWiki.
  * 
  * @package	Actions
- * @version $Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @version	$Id$
+ * @license	http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
  * 
  * @uses	Wakka::GetConfigValue()
+ * @uses	Config::$wakka_name
  */
-echo $this->GetConfigValue("wakka_name");
+echo $this->GetConfigValue('wakka_name');
 ?>
