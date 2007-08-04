74479ba314acc6ef641720def48434a059aa4edb
Marjolein Katsma <javawoman@wikkawiki.org>
2007-08-04 15:54:32 +0000

    - implemented StaticHref() for Flash url (requires new implementation of StaticHref(), to follow) refs #496

--- actions/flash/flash.php
+++ actions/flash/flash.php
@@ -1,19 +1,23 @@
 <?php
 /**
- * Embed a flash object in a wiki page.
+ * Embed a Flash object in a wiki page.
  * 
  * Example: {{flash url="http://example.com/example.swf" [width=x] [height=x]}}
  *
  * @package 	Actions
- * @version	$Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @name		Flash
+ * @version		$Id$
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
  * 
- * @input 	string $url mandatory: URL of the flash object
- * @input 	int $width optional: custom width (pixels)
- * @input 	int $height optional: custom height (pixels)
- * @output 	displays the flash animation
  * @uses		Wakka::cleanUrl()
+ * @uses		Wakka::StaticHref()
+ *
+ * @input	string	$url	mandatory: URL of the Flash object (or a relative path)
+ * @input	int		$width	optional: custom width (pixels)
+ * @input	int		$height	optional: custom height (pixels)
+ * @output	string	object element wich embeds the Flash object
+ * @todo	use constants instead of "magic numbers"
  */
 
 if (!$width) $width = 550;
@@ -25,7 +29,7 @@ else $height = (int)$height;
 if ($height>950) $height = 950;
 
 if (!$url) $url = $vars[0];
-$url = $this->cleanUrl(trim($url));
+$url = $this->StaticHref($this->cleanUrl(trim($url)));
 
 if ($url)
   echo '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="'.$width.'" height="'.$height.'">
