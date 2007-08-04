e5b68833715553683de25080945865aa6ef20291
Marjolein Katsma <javawoman@wikkawiki.org>
2007-08-04 16:19:10 +0000

    @todo marker added (standardize pages output) #refs #496

--- actions/orphanedpages/orphanedpages.php
+++ actions/orphanedpages/orphanedpages.php
@@ -4,18 +4,19 @@
  * 
  * @package Actions
  * @version	$Id$
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @license	http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @filesource
  * 
  * @uses	Wakka::LoadOrphanedPages()
  * @uses	Wakka::Link()
+ * @todo	use new array lister method(s) to generate unordered list or columns
  */
 
 if ($pages = $this->LoadOrphanedPages())
 {
 	foreach ($pages as $page)
 	{
-		print $this->Link($page["tag"], "", "", 0)."<br />\n";
+		print $this->Link($page['tag'], '', '', 0)."<br />\n";
 	}
 }
 else
