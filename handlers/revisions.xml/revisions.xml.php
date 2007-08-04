542d414f5ae65965fe75b470b1fc47d342795d01
Marjolein Katsma <javawoman@wikkawiki.org>
2007-08-04 18:08:46 +0000

    - cleanup (coding guidelines) - group doc for constants refs #496

--- handlers/revisions.xml/revisions.xml.php
+++ handlers/revisions.xml/revisions.xml.php
@@ -17,25 +17,26 @@
  * @todo		use FeedCreator
  */
 
-/**
- * Defaults
+/**#@+
+ * Default value
  */
 if (!defined('I18N_LANG')) define('I18N_LANG', 'en-US');
 if (!defined('I18N_ENCODING_UTF8')) define('I18N_ENCODING_UTF8', 'UTF-8');
 if (!defined('RSS_REVISIONS_VERSION')) define('RSS_REVISIONS_VERSION','2.0');
 if (!defined('RSS_RECENTCHANGES_VERSION')) define('RSS_RECENTCHANGES_VERSION','0.92');
+/**#@-*/
 
 header("Content-type: text/xml");
 
 $xml = '<?xml version="1.0" encoding="'.I18N_ENCODING_UTF8.'"?>'."\n";
 $xml .= '<rss version="'.RSS_REVISIONS_VERSION.'">'."\n";
 $xml .= "<channel>\n";
-$xml .= "<title>".$this->GetConfigValue('wakka_name')." - ".$this->tag."</title>\n";
-$xml .= "<link>".$this->Href()."</link>\n";
+$xml .= '<title>'.$this->GetConfigValue('wakka_name').' - '.$this->tag."</title>\n";
+$xml .= '<link>'.$this->Href()."</link>\n";
 $xml .= '<description>'.sprintf(RECENTCHANGES_LABEL, $this->GetConfigValue('wakka_name').'/'.$this->tag)."</description>\n";
 $xml .= '<language>'.I18N_LANG."</language>\n";
 
-if ($this->HasAccess("read"))
+if ($this->HasAccess('read'))
 {
 	// load revisions for this page
 	if ($pages = $this->LoadRevisions($this->tag))
@@ -43,10 +44,10 @@ if ($this->HasAccess("read"))
 		foreach ($pages as $page)
 		{
 			$xml .= "<item>\n";
-			$xml .= "<title>".$page['time']."</title>\n";
+			$xml .= '<title>'.$page['time']."</title>\n";
 			$xml .= '<link>'.$this->Href('show', '', 'time='.urlencode($page['time'])).'</link>'."\n";
 			$xml .= '<description>'.sprintf(REVISIONS_EDITED_BY, $this->htmlspecialchars_ent($page['user'],ENT_COMPAT,'XML')).($page['note'] ? ' - '.$this->htmlspecialchars_ent($page['note'],ENT_COMPAT,'XML') : '')."</description>\n";
-			$xml .= "\t<pubDate>".date("r",strtotime($page['time']))."</pubDate>\n";
+			$xml .= '<pubDate>'.date('r',strtotime($page['time']))."</pubDate>\n";
 			$xml .= "</item>\n";
 		}
 	}
@@ -55,7 +56,7 @@ else
 {
 	$xml .= "<item>\n";
 	$xml .= '<title>'.WIKKA_ERROR_CAPTION.'</title>'."\n";
-	$xml .= "<link>".$this->Href("show")."</link>\n";
+	$xml .= '<link>'.$this->Href()."</link>\n";
 	$xml .= '<description>'.WIKKA_ERROR_ACL_READ_INFO."</description>\n";
 	$xml .= "</item>\n";
 }
