c596bbdf0fbbf8bffc7cd2308c133e17f8204d01
Dario Taraborelli <dartar@wikkawiki.org>
2007-10-19 17:49:46 +0000

    General cleanup, applying coding guidelines, fixing alignment of feed icon, refs #146 and #562 and closes #572

--- templates/footer.php
+++ templates/footer.php
@@ -1,61 +1,67 @@
 <div class="footer">
 <?php 
-	echo $this->FormOpen("", "TextSearch", "get"); 
-	echo $this->HasAccess("write") ? "<a href=\"".$this->href("edit")."\" title=\"Click to edit this page\">Edit page</a> ::\n" : "";
-	echo "<a href=\"".$this->href("history")."\" title=\"Click to view recent edits to this page\">Page History</a> ::\n";
-	echo $this->GetPageTime() ? "<a href=\"".$this->href("revisions")."\" title=\"Click to view recent revisions list for this page\">".$this->GetPageTime()."</a> <a href=\"".$this->href("revisions.xml")."\" title=\"Click to view recent page revisions in XML format.\"><img src=\"images/xml.png\" width=\"36\" height=\"14\" align=\"middle\" style=\"border : 0px;\" alt=\"XML\" /></a> ::\n" : "";
+	echo $this->FormOpen('', 'TextSearch', 'get'); 
+	echo $this->HasAccess('write') ? '<a href="'.$this->Href('edit').'" title="Click to edit this page">Edit</a> ::'."\n" : '';
+	echo '<a href="'.$this->Href('history').'" title="Click to view recent edits to this page">Page History</a> ::'."\n";
+	echo $this->GetPageTime() ? '<a href="'.$this->Href('revisions').'" title="Click to view recent revisions list for this page">'.$this->GetPageTime().'</a> <a href="'.$this->Href('revisions.xml').'" title="Click to display a feed with the latest revisions to this page."><img src="images/xml.png" width="36" height="14" style="border:0px; vertical-align:middle" alt="feed icon" /></a> ::'."\n" : '';
 
 	// if this page exists
 	if ($this->page)
 	{
 		if ($owner = $this->GetPageOwner())
 		{
-			if ($owner == "(Public)")
+			if ($owner == '(Public)')
 			{
-				print("Public page ".($this->IsAdmin() ? "<a href=\"".$this->href("acls")."\">(Edit ACLs)</a> ::\n" : "::\n"));
+				echo "Public page ".($this->IsAdmin() ? '<a href="'.$this->Href('acls').'">(Edit ACLs)</a> ::'."\n" : '::'."\n");
 			}
 			// if owner is current user
 			elseif ($this->UserIsOwner())
 			{
-           			if ($this->IsAdmin())
-           			{
-					print("Owner: ".$this->Link($owner, "", "", 0)." :: <a href=\"".$this->href("acls")."\">Edit ACLs</a> ::\n");
-            		} 
-            		else 
-            		{
-					print("You own this page. :: <a href=\"".$this->href("acls")."\">Edit ACLs</a> ::\n");
+           		if ($this->IsAdmin())
+           		{
+					echo 'Owner: '.$this->Link($owner, '', '', 0).' :: <a href="'.$this->Href('acls').'">Edit ACLs</a> ::'."\n";
+				} 
+				else
+ 				{
+					echo 'You own this page. :: <a href="'.$this->Href('acls').'">Edit ACLs</a> ::'."\n";
 				}
 			}
 			else
 			{
-				print("Owner: ".$this->Link($owner, "", "", 0)." ::\n");
+				echo 'Owner: '.$this->Link($owner, '', '', 0).' ::'."\n";
 			}
 		}
 		else
 		{
-			print("Nobody".($this->GetUser() ? " (<a href=\"".$this->href("claim")."\">Take Ownership</a>) ::\n" : " ::\n"));
+			echo 'Nobody'.($this->GetUser()? ' (<a href="'.$this->Href('claim').'">Take Ownership</a>) ::'."\n" : ' ::'."\n");
 		}
 	}
 ?>
-<?php echo ($this->GetUser() ? "<a href='".$this->href("referrers")."' title='Click to view a list of URLs referring to this page.'>Referrers</a> :: " : "") ?> 
-Search: <input name="phrase" size="15" class="searchbox" />
-<?php echo $this->FormClose(); ?>
-</div>
-
-<div class="smallprint">
-<?php echo $this->Link("http://validator.w3.org/check/referer", "", "Valid XHTML 1.0 Transitional") ?> ::
-<?php echo $this->Link("http://jigsaw.w3.org/css-validator/check/referer", "", "Valid CSS") ?> ::
-Powered by <?php echo $this->Link("http://wikkawiki.org/", "", "Wikka Wakka Wiki ".$this->GetWakkaVersion()) ?>
-</div>
-
 <?php
-	if ($this->GetConfigValue("sql_debugging"))
+echo ($this->GetUser() ? '<a href="'.$this->Href('referrers').'" title="Click to view a list of URLs referring to this page.">Referrers</a> :: ' : '')."\n";
+?> 
+Search: <input name="phrase" size="15" class="searchbox" />
+<?php
+echo $this->FormClose();
+?>
+</div>
+<div class="smallprint">
+<?php
+echo $this->Link('http://validator.w3.org/check/referer', '', 'Valid XHTML 1.0 Transitional');
+?> ::
+<?php
+echo $this->Link('http://jigsaw.w3.org/css-validator/check/referer', '', 'Valid CSS');
+?> ::
+Powered by <?php echo $this->Link('http://wikkawiki.org/', '', 'Wikka Wakka Wiki '.$this->GetWakkaVersion()); ?>
+</div>
+<?php
+if ($this->GetConfigValue('sql_debugging'))
+{
+	echo '<div class="smallprint"><strong>Query log:</strong><br />'."\n";
+	foreach ($this->queryLog as $query)
 	{
-		print("<div class=\"smallprint\"><strong>Query log:</strong><br />\n");
-		foreach ($this->queryLog as $query)
-		{
-			print($query["query"]." (".$query["time"].")<br />\n");
-		}
-		print("</div>");
+		echo $query['query'].' ('.$query['time'].')<br />'."\n";
 	}
+	echo '</div>'."\n";
+}
 ?>
