0231752654ef88c787d82611a5195705ff25da9a
Dario Taraborelli <dartar@wikkawiki.org>
2008-02-19 23:21:15 +0000

    Uploading past releases as tags, fixes #661

---
+++ actions/table.php
@@ -0,0 +1,48 @@
+<?php
+
+//$vars = array('columns' => '3', 'cellpadding' => '1', 'cells' => '**BIG**;**GREEN**;**FROGS**;yes;yes;no;no;yes;yes');
+
+// Init:
+$delimiter=';';
+$empty_cell='###';
+$row=1;
+$cellpadding=1;
+$cellspacing=1;
+$border=1;
+$columns=1;
+// $style='border-spacing: 2px;border:2px outset #876;width:auto;margin:0 auto;';
+$style='';
+
+// Parameter aus Array holen
+if (is_array($vars))
+{
+    foreach ($vars as $param => $value)
+    {    	
+    	if ($param == 'style') {$style=$value;}   
+    	if ($param == 'columns') {$columns=$value;}	
+        if ($param == 'cellpadding')
+        {
+        	$cellpadding=$value;
+        	$border=$value;
+        }             
+        if ($param == 'cells') $cells = split($delimiter, $value);
+    }
+// Tabellen-Tag oeffnen:
+	echo "<table cellpadding='".$cellpadding."' cellspacing='".$cellspacing."' border='".$border."' style='".$style."'>\n";
+	foreach ($cells as $cell_item)
+	 	{
+	 		if ($row == 1) echo "   <tr>\n";
+	 		if ($cell_item==$empty_cell) $cell_item='<br />';
+			echo "       <td>".$cell_item."</td>\n";
+			$row ++;
+			if ($row > $columns)
+				{
+					$row = "1";
+					echo "   </tr>\n";
+		    	}
+	 	}
+
+// Tabellen-Tag schliessen
+	echo "</table>";
+}
+?>
