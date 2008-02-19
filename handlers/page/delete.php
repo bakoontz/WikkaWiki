9ea29d21de2e8e3ee261bec20cd5722e1e126b77
Dario Taraborelli <dartar@wikkawiki.org>
2008-02-19 23:21:15 +0000

    Uploading past releases as tags, fixes #661

---
+++ handlers/page/delete.php
@@ -0,0 +1,50 @@
+<div class="page">
+<?php
+
+if ($this->IsAdmin())
+{
+    if ($_POST)
+    {
+        $tag = $this->GetPageTag();
+
+        //  delete the page, comments, related links, acls and referrers 
+        $this->Query("delete from ".$this->config["table_prefix"]."pages where tag = '".mysql_escape_string($tag)."'");
+        $this->Query("delete from ".$this->config["table_prefix"]."comments where page_tag = '".mysql_escape_string($tag)."'");
+        $this->Query("delete from ".$this->config["table_prefix"]."links where from_tag = '".mysql_escape_string($tag)."'");
+        $this->Query("delete from ".$this->config["table_prefix"]."acls where page_tag = '".mysql_escape_string($tag)."'");
+        $this->Query("delete from ".$this->config["table_prefix"]."referrers where page_tag = '".mysql_escape_string($tag)."'");
+
+        $message = "Page has been deleted ";
+
+        // redirect back to main page
+        $this->SetMessage($message."!");
+        $this->Redirect($this->config["base_url"]);
+    }
+    else
+    {
+        // show form
+        ?>
+        <h3>Delete <?php echo $this->Link($this->GetPageTag()) ?></h3>
+        <br />
+
+        <?php echo $this->FormOpen("delete") ?>
+        <table border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td>Completely delete this page, including all comments?</td>
+            </tr>
+            <tr>
+                <td> <!-- nonsense input so form submission works with rewrite mode --><input type="hidden" value="" name="null"><input type="submit" value="Delete Page"  style="width: 120px"   />
+                <input type="button" value="Cancel" onClick="history.back();" style="width: 120px" /></td>
+            </tr>
+        </table>
+        <?php
+        print($this->FormClose());
+    }
+}
+else
+{
+    print("<em>You are not allowed to delete pages.</em>");
+}
+
+?>
+</div>
