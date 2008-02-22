100603b2863c7597b799d25c01f994dab421e349
Mahefa Randimbisoa <dotmg@wikkawiki.org>
2008-02-22 14:28:28 +0000

    COORD of [938] to trunk.
    The value of <input type="hidden" name="wakka" ...> should be HomePage/diff and not HomePage. The problem is inside (advanced) FormOpen().
    
    refs #670

--- libs/Wakka.class.php
+++ libs/Wakka.class.php
@@ -3009,7 +3009,8 @@ if ($debug) echo "<br/>\n";
 		else
 		{
 			$attrAction = ' action="'.$this->Href($handler).'"';
-			$hidden['wakka'] = ('' != $tag) ? $tag : $this->tag;
+			// #670: This value will short-circuit the value of wakka=... in URL.
+			$hidden['wakka'] = $this->MiniHref($handler, ('' == $tag ? $this->GetPageTag(): $tag));
 			// @@@ add passed extra GET params here by adding them as extra
 			// entries to $hidden (probably not by adding them to Href()
 			// but that needs to be tested when we get to it!)
