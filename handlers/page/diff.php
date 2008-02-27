24f602a9609d9dce1921b6e543665433cbe8666a
Dario Taraborelli <dartar@wikkawiki.org>
2008-02-27 01:07:02 +0000

    {{diff}} handler: XHTML validation and semantically correct markup, displaying revision number, edit notes and authors, applying datetime class, fixing layout, refs #580 and #650

--- handlers/page/diff.php
+++ handlers/page/diff.php
@@ -30,31 +30,45 @@
  * 			JW: that may be nice but won't work on a Windows system ;) 
  */
 
-define ('ERROR_DIV_LIBRARY_MISSING', 'The necessary file "libs/diff.lib.php" could not be found. Please make sure the file exists and is placed in the right directory!');
-define ('ERROR_NO_PAGE_ACCESS', 'You are not authorized to view this page.');
-define ('CONTENT_ADDITIONS_HEADER', 'Additions:');
-define ('CONTENT_DELETIONS_HEADER', 'Deletions:');
-define ('CONTENT_NO_DIFFERENCES', 'No Differences');
+if (!defined('ERROR_DIV_LIBRARY_MISSING')) define ('ERROR_DIV_LIBRARY_MISSING', 'The necessary file "libs/diff.lib.php" could not be found. Please make sure the file exists and is placed in the right directory!');
+if (!defined('ERROR_NO_PAGE_ACCESS')) define ('ERROR_NO_PAGE_ACCESS', 'You are not authorized to view this page.');
+if (!defined('CONTENT_ADDITIONS_HEADER')) define ('CONTENT_ADDITIONS_HEADER', 'Additions:');
+if (!defined('CONTENT_DELETIONS_HEADER')) define ('CONTENT_DELETIONS_HEADER', 'Deletions:');
+if (!defined('CONTENT_NO_DIFFERENCES')) define ('CONTENT_NO_DIFFERENCES', 'No Differences');
+if (!defined('WHEN_BY_WHO')) define('WHEN_BY_WHO', '%1$s by %2$s');
+if (!defined('UNREGISTERED_USER')) define('UNREGISTERED_USER', 'unregistered user');
 
 echo '<div class="page">'."\n"; //TODO: move to templating class //TODO move _after_ redirect
 
-if ($this->HasAccess("read")) 
+if ($this->HasAccess('read')) 
 {
 	// looking for the diff-classes
 	// @@@ TODO needed only if we're NOT doing a 'fastdiff'
 	// instead of copping out we could use fastdiff as fallback if the library is missing:
 	// first determine diff method based on params AND presense of library; then do it
-	if (file_exists('libs'.DIRECTORY_SEPARATOR.'diff.lib.php')) require_once('libs'.DIRECTORY_SEPARATOR.'diff.lib.php');	#89
-	else die(ERROR_DIFF_LIBRARY_MISSING);	// @@@ ERROR: end div won't be produced here
+	if (file_exists('libs'.DIRECTORY_SEPARATOR.'diff.lib.php'))
+	{
+		require_once('libs'.DIRECTORY_SEPARATOR.'diff.lib.php');	#89
+	}
+	else
+	{
+		die(ERROR_DIFF_LIBRARY_MISSING);	// @@@ ERROR: end div won't be produced here
+	}
 
 	// load pages
-	#$pageA = $this->LoadPageById($_REQUEST['a']);
-	#$pageB = $this->LoadPageById($_REQUEST['b']);
 	$pageA = $this->LoadPageById($_GET['a']);	#312
 	$pageB = $this->LoadPageById($_GET['b']);	#312
 
+	$pageA_edited_by = $pageA['user'];
+	if (!$this->LoadUser($pageA_edited_by)) $pageA_edited_by .= ' ('.UNREGISTERED_USER.')';
+	if ($pageA['note']) $noteA='['.$this->htmlspecialchars_ent($pageA['note']).']'; else $noteA ='';
+
+	$pageB_edited_by = $pageB['user'];	
+	if (!$this->LoadUser($pageB_edited_by)) $pageB_edited_by .= ' ('.UNREGISTERED_USER.')';
+	if ($pageB['note']) $noteB='['.$this->htmlspecialchars_ent($pageB['note']).']'; else $noteB ='';
+	
+	
 	// If asked, call original diff 
-	#if (isset($_REQUEST['fastdiff']) && $_REQUEST['fastdiff'])
 	if (isset($_GET['fastdiff']) && $_GET['fastdiff'])	#312
 	{
 		// prepare bodies
@@ -63,34 +77,37 @@ if ($this->HasAccess("read"))
 
 		$added   = array_diff($bodyA, $bodyB);
 		$deleted = array_diff($bodyB, $bodyA);
-	
-		  $output .= "<b>Comparison of  <a href=\"".$this->Href("", "", "time=".urlencode($pageA["time"]))."\">".$pageA["time"]."</a> &amp; <a href=\"".$this->Href("", "", "time=".urlencode($pageB["time"]))."\">".$pageB["time"]."</a></b><br />\n"; #i18n
-	
-		  if ($added)
-		  {
+		
+		$output .= '<h3>Comparing <a title="Display the revision list for '.$pageA['tag'].'" href="'.$this->Href('revisions').'">revisions</a> for <a title="Return to the current revision of the page" href="'.$this->Href().'">'.$pageA['tag'].'</a></h3>'."\n";
+		$output .= '<ul style="margin: 10px 0;">'."\n";
+		$output .= '	<li><a href="'.$this->Href('', '', 'time'.urlencode($pageA['time'])).'">['.$pageA['id'].']</a> '.sprintf(WHEN_BY_WHO, '<a class="datetime" href="'.$this->Href('show','','time='.urlencode($pageA["time"])).'">'.$pageA['time'].'</a>', $pageA_edited_by).' <span class="pagenote smaller">'.$noteA.'</span></li>'."\n";
+		$output .= '	<li><a href="'.$this->Href('', '', 'time'.urlencode($pageB['time'])).'">['.$pageB['id'].']</a> '.sprintf(WHEN_BY_WHO, '<a class="datetime" href="'.$this->Href('show','','time='.urlencode($pageB["time"])).'">'.$pageB['time'].'</a>', $pageB_edited_by).' <span class="pagenote smaller">'.$noteB.'</span></li>'."\n";
+		$output .= '</ul>'."\n";
+		$output .= '<hr />'."\n";
+		if ($added)
+		{
 			// remove blank lines
-			$output .= "<br />\n<b>".CONTENT_ADDITIONS_HEADER."</b><br />\n";
-			$output .= "<span class=\"additions\">".$this->Format(implode("\n", $added))."</span>";
-		  }
+			$output .= "\n".'<h4 class="clear">'.CONTENT_ADDITIONS_HEADER.'</h4>'."\n";
+			$output .= '<span class="additions">'.$this->Format(implode("\n", $added)).'</span>'."\n";
+		}
 	
-		  if ($deleted)
-		  {
-			$output .= "<br />\n<b>".CONTENT_DELETIONS_HEADER."</b><br />\n";
-			$output .= "<span class=\"deletions\">".$this->Format(implode("\n", $deleted))."</span>";
-		  }
+		if ($deleted)
+		{
+			$output .= "\n".'<h4 class="clear">'.CONTENT_DELETIONS_HEADER.'</h4>'."\n";
+			$output .= '<span class="deletions">'.$this->Format(implode("\n", $deleted)).'</span>'."\n";
+		}
 	
-		  if (!$added && !$deleted)
-		  {
-			$output .= "<br />\n".CONTENT_NO_DIFFERENCES;
-		  }
-		  echo $output;
-	
+		if (!$added && !$deleted)
+		{
+			$output .= '<br />'."\n".CONTENT_NO_DIFFERENCES;
+		}
+		echo $output;
 	}
 	else
 	{
 		// extract text from bodies
-		$textA = $pageA["body"];
-		$textB = $pageB["body"];
+		$textA = $pageA['body'];
+		$textB = $pageB['body'];
 	
 		$sideA = new Side($textA);
 		$sideB = new Side($textB);
@@ -109,16 +126,21 @@ if ($this->HasAccess("read"))
 	
 		$sideO = new Side($fmt->format($diff));
 	
-		$resync_left=0;
-		$resync_right=0;
+		$resync_left = 0;
+		$resync_right = 0;
 	
 		$count_total_right=$sideB->getposition() ;
 	
 		$sideA->init();
 		$sideB->init();
-	
-		echo "<b>Comparing <a href=\"".$this->Href("", "", "time=".urlencode($pageA["time"]))."\">".$pageA["time"]."</a> to <a href=\"".$this->Href("", "", "time=".urlencode($pageB["time"]))."\">".$pageB["time"]."</a></b> "; #i18n
-		echo "-- Highlighting Guide: <span class=\"additions\">addition</span> <span class=\"deletions\">deletion</span><p>"; #i18n
+
+		echo '<h3>Comparing <a title="Display the revision list for '.$pageA['tag'].'" href="'.$this->Href('revisions').'">revisions</a> for <a title="Return to the current revision of the page" href="'.$this->Href().'">'.$pageA['tag'].'</a></h3>'."\n";
+		echo '<ul style="margin: 10px 0">'."\n";
+		echo '	<li><a href="'.$this->Href('', '', 'time'.urlencode($pageA['time'])).'">['.$pageA['id'].']</a> '.sprintf(WHEN_BY_WHO, '<a class="datetime" href="'.$this->Href('show','','time='.urlencode($pageA["time"])).'">'.$pageA['time'].'</a>', $pageA_edited_by).' <span class="pagenote smaller">'.$noteA.'</span></li>'."\n";
+		echo '	<li><a href="'.$this->Href('', '', 'time'.urlencode($pageB['time'])).'">['.$pageB['id'].']</a> '.sprintf(WHEN_BY_WHO, '<a class="datetime" href="'.$this->Href('show','','time='.urlencode($pageB["time"])).'">'.$pageB['time'].'</a>', $pageB_edited_by).' <span class="pagenote smaller">'.$noteB.'</span></li>'."\n";
+		echo '</ul>'."\n";
+		echo '<p>Highlighting Guide: <span class="additions">addition</span> <span class="deletions">deletion</span></p>'."\n"; #i18n
+		echo '<hr />'."\n";
 		$output='';
 
 		  while (1) {
@@ -183,8 +205,9 @@ if ($this->HasAccess("read"))
 	}
 
 }
-else{
-	echo '<em class="error">'.ERROR_NO_PAGE_ACCESS.'</em>';
+else
+{
+	echo '<em class="error">'.ERROR_NO_PAGE_ACCESS.'</em>'."\n";
 }
 ?>
 </div>
