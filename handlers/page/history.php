0a874061cd0336a746d8347af348ed7c0551384b
Dario Taraborelli <dartar@wikkawiki.org>
2008-03-10 08:54:43 +0000

    Applying coding guidelines, semantically correct markup and fixing minor issues in the history handler, refs #701 (parts 1 and 2)

--- handlers/page/history.php
+++ handlers/page/history.php
@@ -9,20 +9,23 @@ if (!defined('HISTORY_PAGE_VIEW'))  define('HISTORY_PAGE_VIEW', 'Page view:');
 if (!defined('OLDEST_VERSION_EDITED_ON_BY'))  define('OLDEST_VERSION_EDITED_ON_BY', 'Oldest known version of this page was edited on %1$s by %2$s');
 if (!defined('MOST_RECENT_EDIT'))  define('MOST_RECENT_EDIT', 'Most recent edit on %1$s by %2$s');
 if (!defined('UNREGISTERED_USER')) define('UNREGISTERED_USER', 'unregistered user');
-
 ?>
 <div class="page">
 <?php
-if ($this->HasAccess("read")) {
+if ($this->HasAccess('read'))
+	{
 	// load revisions for this page
 	if ($pages = $this->LoadRevisions($this->tag))
 	{
-		if ($user = $this->GetUser()) {
-			$max = $user["revisioncount"];
-		} else {
+		if ($user = $this->GetUser())
+		{
+			$max = $user['revisioncount'];
+		}
+		else
+		{
 			$max = 20;
 		}
-		$output = "";
+		$output = '';
 		$c = 0;
 		foreach ($pages as $page)
 		{
@@ -39,12 +42,13 @@ if ($this->HasAccess("read")) {
 
 					// check if the loaded pages are actually revisions of the current page! (fixes #0000046)
 					//print_r($pageA); print_r($this->page); exit;
-					if (($pageA['tag'] != $this->page['tag']) || ($pageB['tag'] != $this->page['tag'])) {
+					if (($pageA['tag'] != $this->page['tag']) || ($pageB['tag'] != $this->page['tag']))
+					{
 						$allowed = 0;
 					}
 					// show if we're still allowed to see the diff
-					if ($allowed) {
-
+					if ($allowed)
+					{
 						// prepare bodies
 						$bodyA = explode("\n", $pageA["body"]);
 						// $bodyB = explode("\n", $pageB["body"]);
@@ -52,26 +56,35 @@ if ($this->HasAccess("read")) {
 						$added = array_diff($bodyA, $bodyB);
 						$deleted = array_diff($bodyB, $bodyA);
 
-      if (strlen($pageA['note']) == 0) $note = ''; else $note = '['.$this->htmlspecialchars_ent($pageA['note']).']';
-
-						if ($c == 2) {
-							$output .= '<strong>'.sprintf(MOST_RECENT_EDIT, '<a href="'.$this->Href('', '', 'time='.urlencode($pageA['time'])).'">'.$pageA['time'].'</a>', $EditedByUser).'</strong> <span class="pagenote smaller">'.$note."</span><br />\n";
+						if (strlen($pageA['note']) == 0)
+						{
+							$note = '';
 						}
-						else {
-							$output .= '<strong>'.sprintf(EDITED_ON,        '<a href="'.$this->Href('', '', 'time='.urlencode($pageA['time'])).'">'.$pageA['time'].'</a>', $EditedByUser).'</strong> <span class="pagenote smaller">'.$note."</span><br />\n";
+						else
+						{
+							$note = '['.$this->htmlspecialchars_ent($pageA['note']).']';
+						}
+
+						if ($c == 2)
+						{
+							$output .= '<h4 class="clear">'.sprintf(MOST_RECENT_EDIT, '<a href="'.$this->Href('', '', 'time='.urlencode($pageA['time'])).'">'.$pageA['time'].'</a>', $EditedByUser).'</h4> <span class="pagenote smaller">'.$note."</span><br />\n";
+						}
+						else
+						{
+							$output .= '<h4 class="clear">'.sprintf(EDITED_ON, '<a href="'.$this->Href('', '', 'time='.urlencode($pageA['time'])).'">'.$pageA['time'].'</a>', $EditedByUser).'</h4> <span class="pagenote smaller">'.$note."</span><br />\n";
 						}
 
 						if ($added)
 						{
 							// remove blank lines
-							$output .= "<br />\n<strong>".DIFF_ADDITIONS."</strong><br />\n";
-							$output .= "<span class=\"additions\">".$this->Format(implode("\n", $added))."</span><br />";
+							$output .= '<br />'."\n".'<h5 class="clear">'.DIFF_ADDITIONS.'</h5>'."\n";
+							$output .= '<div class="additions">'.$this->Format(implode("\n", $added)).'</div><br />';
 						}
 
 						if ($deleted)
 						{
-							$output .= "<br />\n<strong>".DIFF_DELETIONS."</strong><br />\n";
-							$output .= "<span class=\"deletions\">".$this->Format(implode("\n", $deleted))."</span><br />";
+							$output .= '<br />'."\n".'<h5 class="clear">'.DIFF_DELETIONS.'</h5>'."\n";
+							$output .= '<div class="deletions">'.$this->Format(implode("\n", $deleted)).'</div><br />';
 						}
 
 						if (!$added && !$deleted)
@@ -81,21 +94,34 @@ if ($this->HasAccess("read")) {
 						$output .= "<br />\n<hr /><br />\n";
 					}
 				}
-				else {
+				else
+				{
 					// $output .= "<DIV class=\"revisioninfo\">Current page:</div>".$this->Format($pageB["body"])."<br />\n<HR><br />\n";
 				}
 				$pageA = $this->LoadPageById($page["id"]);
 				$EditedByUser  = $page['user'];	
-				if (!$this->LoadUser($EditedByUser)) $EditedByUser .= ' ('.UNREGISTERED_USER.')';				
+				if (!$this->LoadUser($EditedByUser))
+				{
+					$EditedByUser .= ' ('.UNREGISTERED_USER.')';				
+				}
 			}
 		}
-		if ($page['note']) $note='['.$this->htmlspecialchars_ent($page['note']).']'; else $note ='';
-		$output .= '<strong>'.sprintf(OLDEST_VERSION_EDITED_ON_BY, '<a href="'.$this->href('', '', 'time='.urlencode($pageB['time'])).'">'.$pageB['time'].'</a>', $EditedByUser).'</strong> <span class="pagenote smaller">'.$note."</span></strong><br />\n";
+		if ($page['note'])
+		{
+			$note='['.$this->htmlspecialchars_ent($page['note']).']';
+		}
+		else
+		{
+			$note ='';
+		}
+		$output .= '<h4>'.sprintf(OLDEST_VERSION_EDITED_ON_BY, '<a href="'.$this->href('', '', 'time='.urlencode($pageB['time'])).'">'.$pageB['time'].'</a>', $EditedByUser).'</h4> <span class="pagenote smaller">'.$note."</span><br />\n";
 		$output .= '<div class="revisioninfo">'.HISTORY_PAGE_VIEW.'</div>'.$this->Format(implode("\n", $bodyB));
-		print($output);
+		echo $output;
 	}
-} else {
-	print('<em class="error">'.ERROR_ACL_READ.'</em>');
+}
+else
+{
+	echo '<em class="error">'.ERROR_ACL_READ.'</em>';
 }
 ?>
 </div>
