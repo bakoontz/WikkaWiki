fce347efb84facd049dcc396ef0512355127ade7
Brian Koontz <brian@wikkawiki.org>
2008-03-18 19:43:12 +0000

    Modified link pointing to WikkaReleaseNotes page on docs server. Refs #562

--- actions/wikkachanges.php
+++ actions/wikkachanges.php
@@ -2,7 +2,7 @@
 
 $output = "=====Wikka Release Notes=====\n\n".
 "This server is running [[http://wikkawiki.org/ Wikka Wiki]] version ##{{wikkaversion}}##.\n".
-"For detailed release notes please check the [[http://wikkawiki.org/WikkaReleaseNotes Wikka website]].";
+"For detailed release notes please check the [[http://docs.wikkawiki.org/WikkaReleaseNotes Wikka website]].";
 
 print $this->Format($output);
 
