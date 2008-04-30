7525a104095788ee8aca665e56cea5d8c1924ec6
Brian Koontz <brian@wikkawiki.org>
2008-04-30 03:22:48 +0000

    Standard phpdoc header and lang strings added; user-friendly error message displayed if target page not accessible. Refs #710, #749.

--- actions/include.php
+++ actions/include.php
@@ -1,6 +1,23 @@
 <?php
+/**
+ * Include contents of a target page at the point of invocation 
+ *
+ * @package		Actions
+ * @version		$Id$
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @uses		Wakka::HasAccess()
+ * @uses		Wakka::LoadPage()
+ * @uses		Wakka::Format()
+ */
+
+// defaults
+if(!defined('ERROR_CIRCULAR_REF')) define('ERROR_CIRCULAR_REF', 'Circular reference detected');
+if(!defined('ERROR_TARGET_ACL')) define('ERROR_TARGET_ACL', "You aren't allowed to read included page <tt>%s</tt>");
 
 if (!$page) $page = $wikka_vars;
+$orig_page = $page;
 $page = strtolower($page);
 if (!$this->config["includes"]) $this->config["includes"][] = strtolower($this->tag);
 
@@ -10,6 +27,7 @@ if (!in_array($page, $this->config["includes"]) && $page != $this->tag) {
         	$page = $this->LoadPage($page);
 		print $this->Format($page["body"]);
 	}
-} else print "<em class='error'>Circular reference detected</em>";
+	else printf("<em class='error'>".ERROR_TARGET_ACL."</em>", $orig_page);
+} else print "<em class='error'>".ERROR_CIRCULAR_REF."</em>";
 
 ?>
