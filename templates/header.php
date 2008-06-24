0d38d191617ef2f8b88d9770ac6bdb5f78f75d1a
Nils Lindenberg <nilslindenberg@wikkawiki.org>
2008-06-24 09:35:22 +0000

    closes #781: Better code layout for template/header.php

--- templates/header.php
+++ templates/header.php
@@ -43,19 +43,31 @@ if (!defined('RSS_REVISIONS_VERSION')) define('RSS_REVISIONS_VERSION','2.0');
 if (!defined('RSS_RECENTCHANGES_VERSION')) define('RSS_RECENTCHANGES_VERSION','0.92');
 /**#@-*/
 
-// get "input" variables
+/**#@+
+ * Get "input" variables.
+ */
+// message could have been set by other page/handler 
 $message = $this->GetRedirectMessage();
 $message = addslashes($this->htmlspecialchars_ent($message));
+/**#@-*/
 
-// init output variables
+/**#@+
+ * init output variables.
+ */
 $onload = ('' != $message) ? " onload=\"alert('".$message."');\" " : '';
 $doctitle = sprintf(GENERIC_DOCTITLE, $this->htmlspecialchars_ent($this->GetWakkaName(), ENT_NOQUOTES), $this->htmlspecialchars_ent($this->PageTitle(), ENT_NOQUOTES));
 #$site_base = $this->GetConfigValue('base_url');
 #$site_base = $this->base_url;	// base_url: no longer needed with StaticHref()
+
+// Init Cascading Style Sheets (CSS)
 $href_main_stylesheet = $this->StaticHref('css/'.$this->GetConfigValue('stylesheet'));
 $href_comment_stylesheet = $this->StaticHref('css/'.$this->GetConfigValue('comment_stylesheet'));
 $href_print_stylesheet = $this->StaticHref('css/print.css');
+
+// Init FavIcon
 $href_favicon = $this->StaticHref('images/favicon.ico');
+
+// Init Metadata: robots, keywords, description
 $extra_meta = '';
 if ($this->GetHandler() != 'show' || $this->page['latest'] == 'N' || $this->page['tag'] == 'SandBox')
 {
@@ -69,8 +81,12 @@ if ('' != ($meta_description = $this->GetConfigValue('meta_description')))
 {
 	$extra_meta .= '	<meta name="description" content="'.$meta_description.'" />'."\n";
 }
+
+// Init pagename 
 $pagetag = $this->GetPageTag();
 $pagetag_disp = $this->htmlspecialchars_ent($pagetag);
+
+// Init RSS autodiscovery
 $rss_links = '';
 if ($this->GetHandler() != 'edit' && $this->GetConfigValue('enable_rss_autodiscovery') != 0)	// @@@
 {
@@ -78,10 +94,14 @@ if ($this->GetHandler() != 'edit' && $this->GetConfigValue('enable_rss_autodisco
 	$rss_links .= '	<link rel="alternate" type="application/rss+xml" title="'.sprintf(RSS_REVISIONS_TITLE,$wikiname,$pagetag_disp).' (RSS '.RSS_REVISIONS_VERSION.')" href="'.$this->Href('revisions.xml', $pagetag).'" />'."\n";
 	$rss_links .= '	<link rel="alternate" type="application/rss+xml" title="'.sprintf(RSS_RECENTCHANGES_TITLE,$wikiname).' (RSS '.RSS_RECENTCHANGES_VERSION.')" href="'.$this->Href('recentchanges.xml', $pagetag).'" />'."\n"; // @@@ pagetag needed for Href??
 }
+
+// Init Page title + backlinks-link
 $homepage_url = $this->href('', $this->GetConfigValue('root_page'), '');
 $backlinks_url = $this->href('backlinks', '', '');
 $backlinks_title = sprintf(WIKKA_BACKLINKS_LINK_TITLE, $pagetag_disp);
 $header_heading = '<a id="homepage_link" href="'.$homepage_url.'">'.$this->GetConfigValue('wakka_name').'</a> : '.'<a href="'.$backlinks_url.'" title="'.$backlinks_title.'">'.$pagetag_disp.'</a>';
+
+// Init Navigation
 $header_navlinks = '';
 #if ($user = $this->GetUser())
 if ($this->existsUser())
@@ -95,6 +115,7 @@ else
 	$header_navlinks .= $this->GetConfigValue('navigation_links') ? $this->Format($this->GetConfigValue('navigation_links')) : '';
 	// optionally display user's IP address here
 }
+/**#@-*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -107,8 +128,9 @@ else
 	<link rel="stylesheet" type="text/css" href="<?php echo $href_print_stylesheet; ?>" media="print" />
 	<link rel="icon" href="<?php echo $href_favicon; ?>" type="image/x-icon" />
 	<link rel="shortcut icon" href="<?php echo $href_favicon; ?>" type="image/x-icon" />
-<?php echo $rss_links; ?>
-<?php
+<?php 
+echo $rss_links;
+// output headers added via Wakka::AddCustomHeader()
 if (isset($this->additional_headers) && is_array($this->additional_headers) && count($this->additional_headers) > 0)
 {
 	foreach ($this->additional_headers as $additional_header)
