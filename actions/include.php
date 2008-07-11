04f68baf105d33a3e7307287ee4cfbe59cbf08e2
Nils Lindenberg <nilslindenberg@wikkawiki.org>
2008-07-11 20:03:28 +0000

    refs #784: fixing uninitialized variables +coding style

--- actions/include.php
+++ actions/include.php
@@ -10,24 +10,48 @@
  * @uses		Wakka::HasAccess()
  * @uses		Wakka::LoadPage()
  * @uses		Wakka::Format()
+ * 
+ * @todo	Should not tell the user if he has no right to read the page
  */
 
 // defaults
 if(!defined('ERROR_CIRCULAR_REF')) define('ERROR_CIRCULAR_REF', 'Circular reference detected');
 if(!defined('ERROR_TARGET_ACL')) define('ERROR_TARGET_ACL', "You aren't allowed to read included page <tt>%s</tt>");
 
-if (!$page) $page = $wikka_vars;
-$orig_page = $page;
-$page = strtolower($page);
-if (!$this->config["includes"]) $this->config["includes"][] = strtolower($this->tag);
+$page ='';
 
-if (!in_array($page, $this->config["includes"]) && $page != $this->tag) {
-	if ($this->HasAccess("read", $page)) {
-      	$this->config["includes"][] = $page;
-        	$page = $this->LoadPage($page);
-		print $this->Format($page["body"]);
-	}
-	else printf("<em class='error'>".ERROR_TARGET_ACL."</em>", $orig_page);
-} else print "<em class='error'>".ERROR_CIRCULAR_REF."</em>";
+// getting params
+if (is_array($vars))
+{
+    foreach ($vars as $param => $value)
+    {
+    	if ($param == 'page') 
+    	{
+    		$page = $value;
+    	}
+    }
+}
 
+// compatibilty for {{include SandBox}}
+if (('' == $page) && isset($wikka_vars)) $page = $wikka_vars;
+
+// check for circular reference and include the page
+if('' != $page)
+{
+	$orig_page = $page;
+	$page = strtolower($page);
+	if (!isset($this->config["includes"])) $this->config["includes"][] = strtolower($this->tag);
+	
+	if (!in_array($page, $this->config["includes"]) && $page != $this->tag) 
+	{
+		if ($this->HasAccess("read", $page)) 
+		{
+	      	$this->config["includes"][] = $page;
+	        	$page = $this->LoadPage($page);
+			print $this->Format($page["body"]);
+		}
+		else printf("<em class='error'>".ERROR_TARGET_ACL."</em>", $orig_page);
+	} 
+	else print "<em class='error'>".ERROR_CIRCULAR_REF."</em>";
+}
 ?>
