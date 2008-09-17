b66413e87547323f46b9827c13f411715206a489
Dario Taraborelli <dartar@wikkawiki.org>
2008-09-17 20:38:45 +0000

    {{lastusers}} action code cleanup: (partially ported from trunk, adapted to 1.1.6 branch, added "style" parameter consistently with {{highscores}} action, refs #784

--- actions/lastusers.php
+++ actions/lastusers.php
@@ -1,35 +1,90 @@
 <?php
-// ***** CONSTANTS section *****
-define('NAME', "Name");
-define('OWNED_PAGES', "Owned Pages");
-define('SIGNUP_DATE_TIME', "Signup Date/Time");
-// ***** END CONSTANTS section *****
+/**
+ * Display a table of recently registered users.
+ *
+ * @package		Actions
+ * @version		$Id$
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @uses	Wakka::LoadAll()
+ * @uses	Wakka::getCount()
+ * @uses	Wakka::GetConfigValue()
+ *
+ * @input		integer  $max  optional: number of rows to be displayed;
+ *				default: 10
+ * @input		string  $style  optional (simple|complex): displays a simple table or a table with caption and headers and statistics on the number of pages owned;
+ *				default: "complex"
+ * @output		a table with the last registered users
+ * 
+ * @todo	document usage and parameters
+ */
+ 
+//i18n
+if (!defined('LASTUSERS_CAPTION')) define('LASTUSERS_CAPTION', 'Recently registered users');
+if (!defined('SIGNUP_DATE_TIME')) define('SIGNUP_DATE_TIME', 'Signup Date/Time');
+if (!defined('NAME_TH')) define('NAME_TH', 'Username');
+if (!defined('OWNED_PAGES_TH')) define('OWNED_PAGES_TH', 'Owned pages');
+if (!defined('SIGNUP_DATE_TIME_TH')) define('SIGNUP_DATE_TIME_TH', 'Signup date/time');
 
-if ($stat===0) $limit = 1000;
-else $limit = 100;
+//defaults
+define('LASTUSERS_DEFAULT_STYLE', 'complex'); # consistent parameter naming with HighScores action
+define('LASTUSERS_MAX_USERS_DISPLAY', 10);
 
-if (!$max || $limit<$max)
-  $max = $limit;
+//initialize
+$htmlout = '';
+$style = '';
+$max = '';
 
-$last_users = $this->LoadAll("select name, signuptime from ".$this->config["table_prefix"]."users order by signuptime desc limit ".(int)$max);
-
-$htmlout = "<table width=\"50%\" border=\"0\" cellpadding=\"3%\">\n".
-  "  <tr>\n".
-    "    <th><u>".NAME."</u></th>\n".
-    "    <th><u>".OWNED_PAGES."</u></th>\n".
-    "    <th><u>".SIGNUP_DATE_TIME."</u></th>\n".
-  "  </tr>\n";
-
-foreach($last_users as $user)
+//validate action parameters
+if (isset($vars['style']) && in_array($vars['style'], array('complex','simple')))
 {
- $htmlout .= "  <tr>\n";
- if ($stat!=="0") $num = $this->LoadSingle("select count(*) as n from ".$this->config["table_prefix"]."pages where owner='".$user["name"]."' AND latest = 'Y'");
- $htmlout .= "    <td>".$this->Link($user["name"])."</td>\n    <td>".($stat!=="0"?" . . . . . (".$num["n"].")":"")."</td>\n    <td>(".$user["signuptime"].")</td>\n";
- $htmlout .= "  </tr>\n";
+	$style = $vars['style'];
+}
+else
+{
+	$style = LASTUSERS_DEFAULT_STYLE;	
+}
+if (isset($vars['max']) && $vars['max'] > 0)
+{
+	$max = (int) $vars['max'];
+}
+else
+{
+	$max = LASTUSERS_MAX_USERS_DISPLAY;	
 }
 
-$htmlout .= "</table>\n";
+// @@@TODO reformat query
+$last_users = $this->LoadAll("SELECT name, signuptime FROM ".$this->GetConfigValue('table_prefix')."users ORDER BY signuptime DESC LIMIT ".$max);
 
-print($htmlout);
+$htmlout .= '<table class="data lastusers">'."\n";
+if ($style == 'complex')
+{
+	$htmlout .= '<caption>'.LASTUSERS_CAPTION.'</caption>'."\n";
+	$htmlout .= '  <tr>'."\n";
+	$htmlout .= '    <th>'.NAME_TH.'</th>'."\n";
+	$htmlout .= '    <th>'.OWNED_PAGES_TH.'</th>'."\n";
+	$htmlout .= '    <th>'.SIGNUP_DATE_TIME_TH.'</th>'."\n";
+	$htmlout .= '  </tr>'."\n";
+}
+foreach($last_users as $user)
+{
+	$htmlout .= '  <tr>'."\n";
+	if ($style == 'complex')
+	{
+		$where = "`owner` = '".mysql_real_escape_string($user['name'])."' AND `latest` = 'Y'";
+		$htmlout .= '    <td>'.$user['name'].'</td>'."\n";
+		$htmlout .= '    <td class="number">'.$this->getCount('pages', $where).'</td>'."\n";
+		$htmlout .= '    <td class="datetime">('.$user['signuptime'].')</td>'."\n";
+	}
+	else
+	{
+		$htmlout .= '    <td>'.$user['name'].'</td>'."\n";
+		$htmlout .= '    <td class="datetime">'.$user['signuptime'].'</td>'."\n";
+	}
+	$htmlout .= "  </tr>\n";
+}
 
+$htmlout .= '</table>'."\n";
+echo $htmlout;
 ?>
