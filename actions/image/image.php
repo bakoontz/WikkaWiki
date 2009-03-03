7a70c80b4353844b03c01c9c727283d2651c2339
Brian Koontz <brian@wikkawiki.org>
2009-03-03 02:29:51 +0000

    Ported to trunk. Refs #853.

--- actions/image/image.php
+++ actions/image/image.php
@@ -18,30 +18,55 @@
  * @input	string	$title	optional: title text displayed when mouse hovers above image
  * @input	string	$class	optional: a class for the image
  * @input	string	$alt	optional: an alt text
+ * @input   int     $height optinal: height of the image
+ * @input   int     $width optinal: width of the image
  * @output	string	img element or image link
  * @todo	make alt required (wasn't there a better version already?)
  */
 
+// defaults
 $title = 'WikiImage';	// @@@ don't use title attribute if no title specified
 $class = $link = '';
 $alt = 'image';			// @@@ don't generate a meaningless alt text: invalid!
+$url = '';
+$width = 0;
+$height = 0;
+$attr = '';
 
+// params
 if (is_array($vars))
 {
 	foreach ($vars as $param => $value)
 	{
 		$value = $this->htmlspecialchars_ent($value);
-		if ($param == 'src' and $vars['url'] == '') {$vars['url']=$value;}
+		if ($param == 'src' && $vars['url'] == '') {$vars['url']=$value;}
 		if ($param == 'title') {$title = $this->htmlspecialchars_ent($vars['title']);}
 		if ($param == 'class') {$class = $this->htmlspecialchars_ent($vars['class']);}
 		if ($param == 'alt') {$alt = $this->htmlspecialchars_ent($vars['alt']);}
 		if ($param == 'link') {$link = $this->htmlspecialchars_ent($vars['link']);}
+        if ($param == 'width' && (int)$vars['width'] > 0) {$width = (int)$vars['width'];}
+        if ($param == 'height' && (int)$vars['height'] > 0) {$height = (int)$vars['height'];}
 	}
 }
 $url = $this->StaticHref($this->cleanUrl(trim($this->htmlspecialchars_ent($vars['url']))));
 
-$output = '<img class="'.$class.'" src="'.$url.'" alt="'.$alt.'" title="'.$title.'" />';
+// try to determine image size if given none
+if (0 == $width && 0 == $height) #
+{
+    if (file_exists($url)) $attr = getimagesize($url);
+    if (is_array($attr))
+    {
+        if (0 < $attr[1]) $height = $attr[1];
+        if (0 < $attr[0]) $width = $attr[0];
+    }
+}
 
+// building output
+$output = '<img';
+if ('' != $class) $output .= ' class="'.$class.'"';
+if (0 < $width) $output .= ' width="'.$width.'"';
+if (0 < $height) $output .= ' height="'.$height.'"';
+$output .= ' src="'.$url.'" alt="'.$alt.'" title="'.$title.'" />';
 
 // link?
 if ($link !== '')
@@ -50,6 +75,6 @@ if ($link !== '')
 }
 
 $output = $this->ReturnSafeHTML($output);
-print($output);
+echo $output;
 
 ?>
