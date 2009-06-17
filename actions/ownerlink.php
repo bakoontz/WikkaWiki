799084bc7249147f01ffb70ea8a7ae1d73b9b1c5
Dario Taraborelli <dartar@wikkawiki.org>
2009-06-17 06:54:13 +0000

    Adding missing menulet for footer menu, refs #11 and #891

---
+++ actions/ownerlink.php
@@ -0,0 +1,32 @@
+<?php
+if ($this->page)
+{
+	if ($owner = $this->GetPageOwner())
+	{
+		if ($owner == '(Public)')
+		{
+			echo "Public page ".($this->IsAdmin() ? '<a href="'.$this->Href('acls').'">(Edit ACLs)</a>'."\n" : "\n");
+		}
+		// if owner is current user
+		elseif ($this->UserIsOwner())
+		{
+       		if ($this->IsAdmin())
+       		{
+				echo 'Owner: '.$this->Link($owner, '', '', 0).' (<a href="'.$this->Href('acls').'">Edit ACLs</a>)'."\n";
+			} 
+			else
+ 				{
+				echo 'You own this page (<a href="'.$this->Href('acls').'">Edit ACLs</a>)'."\n";
+			}
+		}
+		else
+		{
+			echo 'Owner: '.$this->Link($owner, '', '', 0)."\n";
+		}
+	}
+	else
+	{
+		echo 'Nobody'.($this->GetUser()? ' (<a href="'.$this->Href('claim').'">Take Ownership</a>)'."\n" : "\n");
+	}
+}
+?>
