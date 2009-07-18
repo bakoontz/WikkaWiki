ef376d02ded66e247b07c58f6e2ec3af3523360c
Dario Taraborelli <dartar@wikkawiki.org>
2009-07-18 12:07:18 +0000

    Adding "who am I" menulet, refs #6 #11 and #891

---
+++ actions/whoami.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Display the current username if signed in or the hostname for anonymous visitors
+ * This menulet can be toggled from the menu configuration files under /config.
+ * 
+ * @todo	Use FormatUser() method when available
+ */
+$currentuser = $this->GetUserName();
+
+if (strlen($currentuser)>0)
+{
+	//user is registered
+	if ($this->GetUser())
+	{
+		echo "You are ".$this->Link($currentuser);
+	}
+	//user is anonymous
+	else
+	{
+		echo "Hello <tt>".$currentuser."</tt>";
+	}
+}
+?>
