ff75d51d47b4e0cfd787d6f926b8c8a0f25e2285
Brian Koontz <brian@wikkawiki.org>
2009-08-15 02:53:41 +0000

    Modified to remind user to make 'config' directory writeable during upgrade. Refs #6.

--- setup/writeconfig.php
+++ setup/writeconfig.php
@@ -12,6 +12,20 @@ unset($config["footer_action"]);
 unset($config["external_link_tail"]);
 
 // Parse navlinks, convert to menu configuration files, #891 (since 1.2)
+// Check to see if config is writeable...
+if(!is_writeable('config'))
+{
+	print("<p><span class=\"failed\">WARNING:</span> The <tt>config</tt> directory is not writeable; therefore, conversion of old navigation links to new menu items cannot be performed. Please give your web server temporary write access to the <tt>config</tt> directory (<tt>chmod 777 config</tt>; don't forget to remove write access later, i.e. <tt>chmod 755 config</tt>). If you are unable to make this directory writeable, you will need to manually edit the menu files in the <tt>config</tt> directory.  You also need to remove the <tt>navigation_links</tt> and <tt>logged_in_navigation_links</tt> parameters in your <tt>wikka.config.php</tt> file. If you are still having difficulties, please visit <a href=\"http://docs.wikkawiki.org/WikkaInstallation\">WikkaInstallation</a>.</p>\n"); 
+	?>
+	<form action="<?php echo myLocation() ?>?installAction=writeconfig" method="post">
+	<input type="hidden" name="config" value="<?php echo Wakka::hsc_secure(serialize($config2)) ?>" /><?php /*  #427 */ ?>
+	<input type="submit" value="Try again" />
+	</form>	
+	<?php
+	return;	
+}
+else
+{
 $path = 'config'.DIRECTORY_SEPARATOR;
 if(isset($wakkaConfig['navigation_links'])){
 	$navlinks = $wakkaConfig['navigation_links'];
