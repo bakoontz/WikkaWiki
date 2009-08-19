816e3c9c00103b67f39e669c414789ccb7db4987
Dario Taraborelli <dartar@wikkawiki.org>
2009-08-19 12:37:27 +0000

    patch for homepage link menulet, refs #11 and #891

--- actions/homepagelink.php
+++ actions/homepagelink.php
@@ -2,5 +2,5 @@
 /**
  * Homepage link menulet
  */
-echo '<a href="'.$this->config['root_page'].'">'.$this->config['root_page'].'</a>'."\n";
+echo '<a href="'.$this->href('', $this->config['root_page'], '').'">'.$this->config['root_page'].'</a>'."\n";
 ?>
