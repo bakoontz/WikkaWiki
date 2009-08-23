e827e0a2472b8594b05d4c8db6f9895a0f20c561
Dario Taraborelli <dartar@wikkawiki.org>
2009-08-23 08:55:06 +0000

    1st draft of kubrick theme for Wikka, refs #627 and #6

---
+++ templates/kubrick/footer.php
@@ -0,0 +1,31 @@
+<!-- BEGIN PAGE FOOTER -->
+<div id="footer">
+</div>
+<!-- END PAGE FOOTER -->
+<!-- BEGIN SYSTEM INFO -->
+<div id="smallprint">
+<?php
+echo $this->Link('http://validator.w3.org/check/referer', '', 'Valid XHTML 1.0 Transitional');
+?> ::
+<?php
+echo $this->Link('http://jigsaw.w3.org/css-validator/check/referer', '', 'Valid CSS');
+?> ::
+Powered by <?php echo $this->Link('http://wikkawiki.org/', '', 'WikkaWiki ' . ($this->IsAdmin() ? $this->GetWakkaVersion() : "")); ?>
+</div>
+<!-- END SYSTEM INFO -->
+<?php
+if ($this->GetConfigValue('sql_debugging'))
+{
+	echo '<div class="smallprint"><strong>Query log:</strong><br />'."\n";
+	foreach ($this->queryLog as $query)
+	{
+		echo $query['query'].' ('.$query['time'].')<br />'."\n";
+	}
+	echo '</div>'."\n";
+}
+echo '<!--'.sprintf(PAGE_GENERATION_TIME, $this->microTimeDiff($tstart)).'-->'."\n";
+?>
+</div>
+<!-- END PAGE WRAPPER -->
+</body>
+</html>
