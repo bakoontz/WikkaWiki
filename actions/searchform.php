6fae4aa8b5028ec1de520ae51ed292e1e5bdd927
Dario Taraborelli <dartar@wikkawiki.org>
2009-08-29 21:22:27 +0000

    Moving label to constant, semantically improved form, refs #11

--- actions/searchform.php
+++ actions/searchform.php
@@ -2,7 +2,9 @@
 /**
  * Search form menulet
  */
+//i18n
+if (!defined('SEARCHFORM_LABEL')) define('SEARCHFORM_LABEL', 'Search: ');
 echo $this->FormOpen('', 'TextSearch', 'get');
-echo 'Search: <input name="phrase" size="15" class="searchbox" />';
+echo '<label for="searchbox">'.SEARCHFORM_LABEL.'</label><input id="searchbox" name="phrase" size="15" class="searchbox" />';
 echo $this->FormClose();
 ?>
