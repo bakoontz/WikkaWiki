04bfb31bed246f4cffb188110607731412b1cd03
Brian Koontz <brian@wikkawiki.org>
2009-08-29 20:00:24 +0000

    Moved favicons from global images/ dir to individual theme images/ dirs. Refs #6, #627.

--- templates/default/header.php
+++ templates/default/header.php
@@ -23,8 +23,8 @@ if ( substr_count($site_base, 'wikka.php?wakka=') > 0 ) $site_base = substr($sit
 	<meta name="description" content="<?php echo $this->htmlspecialchars_ent($this->GetConfigValue("meta_description")) ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php echo $this->GetThemePath() ?>/css/wikka.css?<?php echo $this->htmlspecialchars_ent($this->GetConfigValue('stylesheet_hash')) ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php echo $this->GetThemePath() ?>/css/print.css" media="print" />
-	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="<?php echo $this->GetThemePath() ?>/images/favicon.ico" type="image/x-icon" />
+	<link rel="shortcut icon" href="<?php echo $this->GetThemePath() ?>/images/favicon.ico" type="image/x-icon" />
 <?php
 if ($this->GetMethod() != 'edit')
 {
