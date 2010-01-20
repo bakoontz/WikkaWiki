a594e66cf3a1bf63fd8273e58557229fb94d7b21
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 16:00:38 +0000

    refs #654, replaced by trunk version

--- actions/colour/colour.php
+++ actions/colour/colour.php
@@ -1,1 +1,17 @@
-<?php include($this->config['action_path'].'/color.php'); ?>
+<?php
+/**
+ * Synonym for color.php.
+ *
+ * @see			color.php
+ *
+ * @package		Actions
+ * @version		$Id:colour.php 369 2007-03-01 14:38:59Z DarTar $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ */
+
+/**
+ * color.php action.
+ */
+include_once($this->BuildFullpathFromMultipath('color'.DIRECTORY_SEPARATOR.'color.php', $this->config['wikka_action_path']));
+?>
