2c6a485b75618766783eb5ff045caaba4f54f2fe
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 16:15:52 +0000

    refs #654 & #959

--- actions/countpages/countpages.php
+++ actions/countpages/countpages.php
@@ -1,8 +1,18 @@
 <?php
-/** 
+/**
  * Print the total number of pages in this wiki.
+ *
+ * @package		Actions
+ * @version		$Id: countpages.php 820 2007-11-23 09:21:08Z DotMG $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @uses	Wakka::getCount()
+ * @uses	Wakka::Link()
  */
-$pagedata = $this->LoadSingle("SELECT count(*) as num FROM ".$this->config["table_prefix"]."pages WHERE latest = 'Y'");
-echo $this->Link('PageIndex', '', $pagedata['num'],'','','Display an alphabetical page index');
+
+$where = "`latest` = 'Y'";
+$count = $this->getCount('pages', $where);
+echo $this->Link('PageIndex', '', $count, '', '', INDEX_LINK_TITLE);
 
 ?>
