ab20182b008a0a43203135e0e0c1cc066833f340
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 22:59:08 +0000

    #refs 654

--- actions/mypages/mypages.php
+++ actions/mypages/mypages.php
@@ -1,28 +1,35 @@
 <?php
 /**
- * Display the list of pages an user owns.
- * 
- * @author	Carlo Zottmann
- * 
- * @uses	Wakka::GetUser()
+ * Display a list of pages owned by the current user.
+ *
+ * If the current user is logged-in and owns at least one page, a list of pages owned by the current user
+ * is displayed, ordered alphabetically or by date and time (last edit first).
+ *
+ * @package		Actions
+ * @version		$Id:mypages.php 369 2007-03-01 14:38:59Z DarTar $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @author	{@link http://web.archive.org/web/20040616194824/http://www.wakkawiki.com/CarloZottmann Carlo Zottmann}
+ *
+ * @uses	Wakka::reg_username
+ * @uses	Wakka::existsUser()
+ * @uses	Wakka::LoadPagesByOwner()
  * @uses	Wakka::GetUserName()
- * @uses	Wakka::htmlspecialchars_ent()
  * @uses	Wakka::IsAdmin()
  * @uses	Wakka::Link()
- * @uses	Wakka::LoadPagesByOwner()
  * 
  */
 
 $username = '';
 if(isset($_GET['user']))
 {
-	$username = $this->htmlspecialchars_ent($_GET['user']);
+	$username = $this->GetSafeVar('user', 'get'); 
 }
-
 if (($this->IsAdmin() && !empty($username)) ||
-	($this->GetUser() && $username = $this->GetUserName()))
+	($this->existsUser() && $username = $this->GetUserName()))
 {
-	printf("<div class='floatl'>".MYPAGES_HEADER."</div><br/><br/>\n", $username);
+	printf('<div class="floatl">'.MYPAGES_HEADER.'</div><br/><br/>'."\n", $username);
 
 	$my_pages_count = 0;
 
@@ -36,15 +43,15 @@ if (($this->IsAdmin() && !empty($username)) ||
 				{
 					$firstChar = "#";
 				}
-	
-				if ($firstChar != $curChar) 
+
+				if ($firstChar != $curChar)
 				{
 					if ($curChar != '') echo "<br />\n";
 					echo '<strong>'.$firstChar."</strong><br />\n";
 					$curChar = $firstChar;
 				}
-	
-				echo $this->Link($page["tag"])."<br />\n";
+
+				echo $this->Link($page['tag'])."<br />\n";
 		}
 	}
 	else
@@ -56,5 +63,4 @@ else
 {
 	echo '<em class="error">'.MYPAGES_NOT_LOGGED_IN.'</em>';
 }
-
 ?>
