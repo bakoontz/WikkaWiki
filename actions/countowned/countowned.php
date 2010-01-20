c8a274d8b6ac0c3a8c292c7f6eaace4f02ea81f0
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 16:14:23 +0000

    refs #654

--- actions/countowned/countowned.php
+++ actions/countowned/countowned.php
@@ -1,11 +1,28 @@
 <?php
 /**
  * Print number of pages owned by the current user.
+ *
+ * @package		Actions
+ * @version		$Id: countowned.php 920 2008-02-18 22:42:38Z BrianKoontz $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @uses	Wakka::GetUserName()
+ * @uses	Wakka::getCount()
+ * @uses	Wakka::Link()
+ * @todo	print different text if user is not logged in
  */
-$str = 'SELECT COUNT(*) FROM '.$this->config["table_prefix"].'pages WHERE `owner` ';
-$str .= "= '" . $this->GetUserName() . "' AND `latest` = 'Y'";
-$countquery = $this->Query($str);
-$count  = mysql_result($countquery, 0);
-echo $this->Link('MyPages', '', $count,'','','Display a list of the pages you currently own');
+/*
+$where = "`owner` = '".mysql_real_escape_string($this->GetUserName())."' AND `latest` = 'Y'";
+$count = $this->getCount('pages', $where);
+*/
+$count = 0;
+if ($username = $this->GetUserName())		// no param: get name of logged in user only (#543)
+{
+	$where = "owner = '".mysql_real_escape_string($username)."' AND latest = 'Y'";
+	$count = $this->getCount('pages',$where);
+}
+
+echo $this->Link('MyPages', '', $count,'','', DISPLAY_MYPAGES_LINK_TITLE);
 
 ?>
