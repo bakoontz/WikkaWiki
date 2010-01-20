15995825d8412d9673b8ae45a0e988e2c50ba3cf
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 21:49:23 +0000

    refs #654, merged trunk & 1.3

--- actions/geshiversion/geshiversion.php
+++ actions/geshiversion/geshiversion.php
@@ -10,35 +10,40 @@
  *
  * @package		Actions
  * @name		GeSHiVersion
- *
+ * @version		$Id: geshiversion.php 1105 2008-05-29 21:32:15Z DarTar $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * 
  * @author		{@link http://wikkawiki.org/DarTar Dario Taraborelli}
  * @author		{@link http://wikkawiki.org/JavaWoman JavaWoman}
  * @author		{@link http://wikkawiki.org/BrianKoontz Brian Koontz}
  * @since		Wikka 1.1.6.2
- *
+ * @filesource
  */
 
 // defaults
-$out = '<abbr title="'.WIKKA_ADMIN_ONLY_TITLE.'">'.NOT_AVAILABLE.'</abbr>'."\n";
+$out = '<abbr title="'.WIKKA_ADMIN_ONLY_TITLE.'">'.WIKKA_NOT_AVAILABLE.'</abbr>'."\n"; 
 
 //check privs
 if ($this->config['public_sysinfo'] == '1' || $this->IsAdmin())
 {
-	if (file_exists($this->config['geshi_path'].'/geshi.php'))
+	if (file_exists($this->GetConfigValue('geshi_path').DIRECTORY_SEPARATOR.'geshi.php'))
 	{
-		include_once($this->config['geshi_path'].'/geshi.php');
+		/**
+		 * GeSHi core.
+		 */
+		include_once($this->GetConfigValue('geshi_path').DIRECTORY_SEPARATOR.'geshi.php');
 		if (defined('GESHI_VERSION'))
 		{
 			$out = GESHI_VERSION;
 		}
 		else
 		{
-			$out = NOT_AVAILABLE;
+			$out = WIKKA_NOT_AVAILABLE;
 		}
 	}
 	else
 	{
-			$out = NOT_INSTALLED;
+			$out = WIKKA_NOT_INSTALLED;
 	}
 }
 echo $out;
