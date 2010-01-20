9b098b0cbfa1381db82e1ff521f54ee8f0876068
Masin Al-Dujaili <masin@wikkawiki.org>
2010-01-20 16:11:53 +0000

    refs #654, changed constant names

--- actions/contact/contact.php
+++ actions/contact/contact.php
@@ -1,6 +1,13 @@
 <?php
 /**
- * Print a spam-safe mailto: link to the administrator's email address. 
+ * Print a spam-safe mailto: link to the administrator's email address.
+ *
+ * @package		Actions
+ * @version		$Id: contact.php 820 2007-11-23 09:21:08Z DotMG $
+ * @license		http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @filesource
+ *
+ * @uses	Wakka::GetConfigValue()
  * 
  * Note: plain mailto links are a common source of spam.
  */
@@ -9,13 +16,13 @@ $email = $this->GetConfigValue("admin_email");
 
 // print spam-safe mailto link
 $patterns = array("'@'", "'\.'");
-$replace = array("[at]", "[dot]"); 
-echo "<a href=\"mailto:".preg_replace($patterns, $replace, $email)."\" title=\"Send us your feedback\">Contact</a>"; #i18n
+$replace = array("[at]", "[dot]");
+echo '<a href="mailto:'.preg_replace($patterns, $replace, $email).'" title="'.CONTACTLINK_TITLE.'">'.CONTACTLINK_TEXT.'</a>';
 
 // print plain mailto link
-//echo "<a href=\"mailto:".$email."\" title=\"Send us your feedback\">Contact</a>"; #i18n
+//echo '<a href="mailto:'.$email.'" title="'.CONTACTLINK_TITLE.'">'.CONTACTLINK_TEXT.'</a>';
 
 // print contact link only to registered users
-// echo ($this->GetUser()) ? "<a href=\"mailto:".$email."\" title=\"Send us your feedback\">Contact</a>" : ""; #i18n
+// echo ($this->GetUser()) ? <a href="mailto:'.$email.'" title="'.CONTACTLINK_TITLE.'">'.CONTACTLINK_TEXT.'</a>' : "";
 
 ?>
