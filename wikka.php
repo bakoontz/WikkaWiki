d1892279e1e1df72b4dd4eff36e4c9f07ff390c7
Brian Koontz <brian@wikkawiki.org>
2013-08-31 16:20:42 +0000

    Fixed XSS vulnerability, affects only sites not running under mod_rewrite. Refs #1153

--- wikka.php
+++ wikka.php
@@ -71,6 +71,112 @@ if (!defined('MINIMUM_MYSQL_VERSION'))	define('MINIMUM_MYSQL_VERSION', '4.1');
 /**#@-*/
 // ----------------------------- END BASIC CONSTANTS ---------------------------
 
+// ----------------------------- HELPER FUNCTIONS ---------------------------
+/**
+ * Shamelessly lifted from libs/Wakka.class.php.  See that file for
+ * documentation, credits, etc.
+ * @see Wakka::hsc_secure()
+**/
+if(!function_exists('hsc_secure'))
+{
+	function hsc_secure($string, $quote_style=ENT_COMPAT)
+	{
+		// init
+		$aTransSpecchar = array('&' => '&amp;',
+								'"' => '&quot;',
+								'<' => '&lt;',
+								'>' => '&gt;'
+								);			// ENT_COMPAT set
+		if (ENT_NOQUOTES == $quote_style)	// don't convert double quotes
+		{
+			unset($aTransSpecchar['"']);
+		}
+		elseif (ENT_QUOTES == $quote_style)	// convert single quotes as well
+		{
+			$aTransSpecchar["'"] = '&#39;';	// (apos) htmlspecialchars() uses '&#039;'
+		}
+
+		// return translated string
+		return strtr($string,$aTransSpecchar);
+	}
+}
+
+/**
+ * Shamelessly lifted from libs/Wakka.class.php.  See that file for
+ * documentation, credits, etc.
+ * @see Wakka::htmlspecialchars_ent()
+**/
+if(!function_exists('htmlspecialchars_ent'))
+{
+	function htmlspecialchars_ent($text,$quote_style=ENT_COMPAT,$doctype='HTML')
+	{
+		// re-establish default if overwritten because of third parameter
+		// [ENT_COMPAT] => 2
+		// [ENT_QUOTES] => 3
+		// [ENT_NOQUOTES] => 0
+		if (!in_array($quote_style,array(ENT_COMPAT,ENT_QUOTES,ENT_NOQUOTES)))
+		{
+			$quote_style = ENT_COMPAT;
+		}
+
+		// define patterns
+		$terminator = ';|(?=($|[\n<]|&lt;))';	// semicolon; or end-of-string, newline or tag
+		$numdec = '#[0-9]+';					// numeric character reference (decimal)
+		$numhex = '#x[0-9a-f]+';				// numeric character reference (hexadecimal)
+		if ($doctype == 'XML')					// pure XML allows only named entities for special chars
+		{
+			// only valid named entities in XML (case-sensitive)
+			$named = 'lt|gt|quot|apos|amp';
+			$ignore_case = '';
+			$entitystring = $named.'|'.$numdec.'|'.$numhex;
+		}
+		else									// (X)HTML
+		{
+			$alpha  = '[a-z]+';					// character entity reference TODO $named='eacute|egrave|ccirc|...'
+			$ignore_case = 'i';					// names can consist of upper and lower case letters
+			$entitystring = $alpha.'|'.$numdec.'|'.$numhex;
+		}
+		$escaped_entity = '&amp;('.$entitystring.')('.$terminator.')';
+
+		// execute our replacement hsc_secure() function, passing on optional parameters
+		$output = hsc_secure($text,$quote_style);
+
+		// "repair" escaped entities
+		// modifiers: s = across lines, i = case-insensitive
+		$output = preg_replace('/'.$escaped_entity.'/s'.$ignore_case,"&$1;",$output);
+
+		// return output
+		return $output;
+	}
+}
+
+/**
+ * Shamelessly lifted from libs/Wakka.class.php.  See that file for
+ * documentation, credits, etc.
+ * @see Wakka::GetSafeVar()
+**/
+if(!function_exists('GetSafeVar'))
+{
+	function GetSafeVar($varname, $gpc='get')
+	{
+		$safe_var = NULL;
+		if ($gpc == 'post')
+		{
+			$safe_var = isset($_POST[$varname]) ? $_POST[$varname] : NULL;
+		}
+		elseif ($gpc == 'get')
+		{
+			$safe_var = isset($_GET[$varname]) ? $_GET[$varname] : NULL;
+		}
+		elseif ($gpc == 'cookie')
+		{
+			$safe_var = isset($_COOKIE[$varname]) ? $_COOKIE[$varname] : NULL;
+		}
+		return (htmlspecialchars_ent($safe_var));
+	}
+}
+// ----------------------------- END HELPER FUNCTIONS --------------------------
+
 // ------------ CRITICAL ERROR MESSAGES USED BEFORE LANG FILE LOADED -----------
 // Do not move these declaration to lang files.
 if(!defined('ERROR_WRONG_PHP_VERSION')) define('ERROR_WRONG_PHP_VERSION', 'Wikka requires PHP %s or higher!');  // %s - version number
@@ -520,7 +626,7 @@ if ($wakkaConfig['wakka_version'] !== WAKKA_VERSION)
 	 * installer (which will receive this data) is passed as a $_GET parameter!
 	 */
 	$installAction = 'default';
-	if (isset($_GET['installAction'])) $installAction = trim($_GET['installAction']);	#312
+	if (isset($_GET['installAction'])) $installAction = trim(GetSafeVar('installAction'));	#312
 	if (file_exists('setup'.DIRECTORY_SEPARATOR.'header.php'))
 	include('setup'.DIRECTORY_SEPARATOR.'header.php'); else print '<em class="error">'.ERROR_SETUP_HEADER_MISSING.'</em>'; #89
 	if
@@ -550,7 +656,7 @@ if(!isset($_SESSION['CSRFToken']))
  *
  * @todo files action uses POST, everything else uses GET #312
  */
-$wakka = $_GET['wakka']; #312
+$wakka = GetSafeVar('wakka'); #312
 
 /**
  * Remove leading slash.
