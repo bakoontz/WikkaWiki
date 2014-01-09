a59c62f0c309ce24729d133e592c2c798f4460c5
klenwell <klenwell@gmail.com>
2014-01-08 21:04:31 -0800

    adds test config file and updates DevTest so that no warnings are now issued

--- test/DevTest.php
+++ test/DevTest.php
@@ -12,21 +12,48 @@
  * - http://phpunit.de/manual/3.7/en/installation.html
  * - http://book.cakephp.org/2.0/en/development/testing.html#installing-phpunit
  */
+require_once('test/test.config.php');
 require_once('libs/Wakka.class.php');
+require_once('version.php');
 
 
 class ReadMeTest extends PHPUnit_Framework_TestCase
 {
+    protected static $pdo;
     protected static $wakka;
+    protected static $config;
  
+    /**
+     * Test Fixtures
+     */
     public static function setUpBeforeClass() {
-        self::$wakka = new Wakka(array());
+        global $wikkaTestConfig;
+        self::$config = $wikkaTestConfig;
+        
+        # create db connection
+        $host = sprintf('mysql:host=%s', self::$config['mysql_host']);
+        self::$pdo = new PDO($host, self::$config['mysql_user'],
+            self::$config['mysql_password']);
+
+        # create database
+        self::$pdo->exec(sprintf('CREATE DATABASE `%s`',
+            self::$config['mysql_database']));
+        self::$wakka = new Wakka(self::$config);
     }
  
     public static function tearDownAfterClass() {
         self::$wakka = NULL;
+        
+        # cleanup database
+        self::$pdo->exec(sprintf('DROP DATABASE `%s`',
+            self::$config['mysql_database']));
+        self::$pdo = NULL;
     }
     
+    
+    /**
+     * Tests
+     */
     public function testWikkaPresence() {
         $this->assertInstanceOf('Wakka', self::$wakka);
     }
