f0cd99670eb93080259cf0e2dd4557b81cfe3aec
PLG <kim@nevelsteen.com>
2019-08-26 22:38:42 +0200

    retab! re-space.

--- formatters/csv.php
+++ formatters/csv.php
@@ -7,34 +7,36 @@
 // And give it the same file permissions as the other files in that directory.
 
 $csv_printheader=0;
-foreach (split("\n", $text) as $csv_n => $csv_line) {
-        if(preg_match("/^#|^\s*$/",$csv_line)) 
-			continue;
+foreach (split("\n", $text) as $csv_n => $csv_line) 
+{
+	if (preg_match("/^#|^\s*$/",$csv_line)) 
+		continue;
 
-        if( $csv_printheader == 0) {
-			print "<table cellpadding=\"5\" cellspacing=\"1\"><tbody>\n"; $csv_printheader=1; 
+	if ($csv_printheader == 0) {
+		print "<table cellpadding=\"5\" cellspacing=\"1\"><tbody>\n"; $csv_printheader=1; 
+	}
+	print ($csv_n%2) ? "<tr bgcolor=\"#ffffee\">" : "<tr bgcolor=\"#eeeeee\">";
+
+	foreach (split(",", $csv_line) as $csv_nn => $csv_cell) 
+	{
+		if (preg_match("/^\s*$/",$csv_cell)) {
+			print "<td>&nbsp;</td>";
+		} elseif (preg_match("/^\"?[\s]*==\/(.*?)\/==\"?$/",$csv_cell,$matches)) {
+			print "<td style=\"background-color:#ccc; text-align:right;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+			$style[$csv_nn]= "text-align:right";
+		} elseif (preg_match("/^\"?[\s]*==\\\\(.*?)\\\\==\"?$/",$csv_cell,$matches)) {
+			print "<td style=\"background-color:#ccc; text-align:left;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+			$style[$csv_nn]= "text-align:left";
+		} elseif (preg_match("/^\"?[\s]*==(.*?)==\"?$/",$csv_cell,$matches)) {
+			print "<td style=\"background-color:#ccc; text-align:center;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+			$style[$csv_nn]= "text-align:center";
+		} elseif (preg_match("/^\"?(.*?)\"?$/",$csv_cell,$matches)) {
+			print "<td style=\"".$style[$csv_nn]."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+		} else {
+			print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
 		}
-        print ($csv_n%2) ? "<tr bgcolor=\"#ffffee\">" : "<tr bgcolor=\"#eeeeee\">";
-
-        foreach(split(",", $csv_line) as $csv_nn => $csv_cell) {
-                if(preg_match("/^\s*$/",$csv_cell)) {
-                        print "<td>&nbsp;</td>";
-                } elseif(preg_match("/^\"?[\s]*==\/(.*?)\/==\"?$/",$csv_cell,$matches)) {
-                        print "<td style=\"background-color:#ccc; text-align:right;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-						$style[$csv_nn]= "text-align:right";
-                } elseif(preg_match("/^\"?[\s]*==\\\\(.*?)\\\\==\"?$/",$csv_cell,$matches)) {
-                        print "<td style=\"background-color:#ccc; text-align:left;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-						$style[$csv_nn]= "text-align:left";
-                } elseif(preg_match("/^\"?[\s]*==(.*?)==\"?$/",$csv_cell,$matches)) {
-                        print "<td style=\"background-color:#ccc; text-align:center;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-						$style[$csv_nn]= "text-align:center";
-                } elseif(preg_match("/^\"?(.*?)\"?$/",$csv_cell,$matches)) {
-                        print "<td style=\"".$style[$csv_nn]."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-                } else {
-                        print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
-                }
-        }
-        print "</tr>\n";
+	}
+	print "</tr>\n";
 }
 if ($csv_printheader == 1) print "</tbody></table>\n";
 ?>
