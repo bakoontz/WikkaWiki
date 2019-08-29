6a010bd85e9fba59095c2978d9baba5fb37d1d6d
PLG <kim@nevelsteen.com>
2019-08-29 15:26:41 +0200

    Fix <td> regex. Show red ERROR on error.

--- formatters/csv.php
+++ formatters/csv.php
@@ -73,17 +73,17 @@ foreach (split("\n", $text) as $csv_n => $csv_line)
 
 		if (preg_match("/^\s*$/",$csv_cell))
 			print "<td>&nbsp;</td>";
-		elseif (preg_match("/^\"?[\s\d+\-,.]+\"?$/", $csv_cell))
+		elseif ($total[$csv_nn] && preg_match("/^\"?([\s\d+\-,.]+)\"?$/", $csv_cell, $matches))
 		{
-			$csv_cell_nows= preg_replace('/\s+/', '', $csv_cell);
+			$matches_nows= preg_replace('/\s+/', '', $matches[1]);
 
-			if (preg_match("/^\"?([+-]?)(\d{1,3}(\.\d{3})*|(\d+)),(\d{2})$\"?$/", $csv_cell_nows, $swe))
+			if (preg_match("/^([+-]?)(\d{1,3}(\.\d{3})*|(\d+)),(\d{2})$/", $matches_nows, $swe))
 			{
 				$format= "SE";
 				$i= $swe[1] . preg_replace('/\./', '', $swe[2]);
 				$d= $swe[1] . $swe[5];
 			}
-			elseif (preg_match("/^\"?([+-]?)(\d{1,3}(\,\d{3})*|(\d+))(\.(\d{2}))?$\"?$/", $csv_cell_nows, $usa))
+			elseif (preg_match("/^([+-]?)(\d{1,3}(\,\d{3})*|(\d+))(\.(\d{2}))?$/", $matches_nows, $usa))
 			{
 				$format= "US";
 				$i= $usa[1] . preg_replace('/,/', '', $usa[2]);
@@ -91,18 +91,19 @@ foreach (split("\n", $text) as $csv_n => $csv_line)
 			}
 			else
 			{
-				$format= "??";
-				$i= "0";
-				$d= "0";
+				$total[$csv_nn]= -1;
+				print "<td style=\"". $style[$csv_nn] ."\">".$this->htmlspecialchars_ent($matches_nows)."</td>";
+				continue;
 			}
 
 			$total_i[$csv_nn]+= intval($i);
 			$total_d[$csv_nn]+= intval($d);
 			$nr= $i + ($d/100);
-			print "<td title=\"". $csv_cell ."(". $format .")\" style=\"". (($nr <= 0) ? "background-color:#d30; " : "" ) . $style[$csv_nn] ."\">". sprintf("%.2f", $nr) ."</td>";
 
 			if ($DEBUG == 1)
-				print "<td style=\"text-align:right;\">". $csv_cell ."(". $format .") &nbsp;". $total_i[$csv_nn] ." ". $total_d[$csv_nn] ."</td>";
+				print "<td style=\"". $style[$csv_nn] ."\">". $csv_cell ."(". $format .")= " . sprintf("%.2f", $nr) ."+= ". $total_i[$csv_nn] ." ". $total_d[$csv_nn] ."</td>";
+			else
+				print "<td title=\"". $csv_cell ."(". $format .")\" style=\"". (($nr <= 0) ? "background-color:#d30; " : "" ) . $style[$csv_nn] ."\">". sprintf("%.2f", $nr) ."</td>";
 		}
 		elseif (preg_match("/^\"?(.*)\"?$/", $csv_cell, $matches))
 		{
@@ -121,7 +122,8 @@ foreach (split("\n", $text) as $csv_n => $csv_line)
 				print "<td style=\"". $style[$csv_nn] ."\">". $this->htmlspecialchars_ent($esc_semicolon) ."</td>";
 		}
 		else
-			print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
+			print "<td style=\"background-color:#d30;". $style[$csv_nn] ."\">ERROR!</td>"; // $this->htmlspecialchars_ent($csv_cell)
+
 	}
 	print "</tr>\n";
 }
