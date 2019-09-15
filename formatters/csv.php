34b1c8ae0e888a6a23f904e0b2be0a8203b4ecec
PLG <xxx@plgd.xxx>
2019-09-15 22:21:31 +0200

    Just some spaces and link to regex.
    Squashed commit of the following:
    
    commit df9367734e70ab310eb520819bffad92da731ee8
    Author: PLG <kim@nevelsteen.com>
    Date:   Wed Aug 28 12:40:35 2019 +0200
    
        Total needs sprintf of 2 decimals. Lighter red color.
    
    commit 9e03899cfdb0470914a33d81461187d19f1fea94
    Author: PLG <kim@nevelsteen.com>
    Date:   Wed Aug 28 11:24:32 2019 +0200
    
        Add DEBUG flag.
    
    commit 94c0ad43d1ab18396eac777ad3350c270a2ed597
    Author: PLG <kim@nevelsteen.com>
    Date:   Tue Aug 27 23:12:35 2019 +0200
    
        Add tooltip for previous value.
    
    commit 4bdf703f2870e63982b4142daeb2c03b8e6e082a
    Author: PLG <kim@nevelsteen.com>
    Date:   Tue Aug 27 22:54:45 2019 +0200
    
        Totals seem to work. Debug ouput left in.

--- formatters/csv.php
+++ formatters/csv.php
@@ -16,16 +16,16 @@ foreach (split("\n", $text) as $csv_n => $csv_line)
 
 	foreach (split(";", $csv_line) as $csv_nn => $csv_cell) 
 	{
+#		https://www.phpliveregex.com
+
 		if (preg_match("/^\"?[\s]*==.*==\"?$/", $csv_cell)) 
 		{
 			$style[$csv_nn]= "padding: 1px 10px 1px 10px; ";
 
 			if (preg_match("/^\"?[\s]*==\/(.*)\/==\"?$/", $csv_cell, $title))
 				$style[$csv_nn].= "text-align:right; ";
-
 			elseif (preg_match("/^\"?[\s]*==\\\\(.*)\\\\==\"?$/", $csv_cell, $title))
 				$style[$csv_nn].= "text-align:left; ";
-
 			elseif (preg_match("/^\"?[\s]*==\|(.*)\|==\"?$/", $csv_cell, $title))
 				$style[$csv_nn].= "text-align:center; ";
 
@@ -36,7 +36,7 @@ foreach (split("\n", $text) as $csv_n => $csv_line)
 		if (preg_match("/^\s*$/",$csv_cell))
 			print "<td>&nbsp;</td>";
 		elseif (preg_match("/^\"?(.*)\"?$/", $csv_cell, $matches))
-			print "<td style=\"". $style[$csv_nn] ."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+			print "<td style=\"". $style[$csv_nn] ."\">". $this->htmlspecialchars_ent($matches[1]) ."</td>";
 		else
 			print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
 	}
