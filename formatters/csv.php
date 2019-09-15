3aac7f62358d255e3bfbdeb403bc106520b74141
PLG <xxx@plgd.xxx>
2019-09-15 21:37:12 +0200

    Change header markers, delimiter. Add alignment.
    Squashed commit of the following:
    
    commit 09ca1198097324ab0af557cd60b68a4ca8857265
    Author: PLG <kim@nevelsteen.com>
    Date:   Fri Aug 31 15:40:29 2018 +0200
    
        Do center and left align as well.
    
    commit e11bc18f905a48eb7ada6671a0f952bab085669b
    Author: PLG <xxx@plgd.xxx>
    Date:   Sun Sep 15 21:13:24 2019 +0200
    
        Allow for right aligned columns.Reformat coding style a bit.
    
    commit 26d4ccf91af45b614e567782e14fcb109e16324d
    Author: PLG <kim@nevelsteen.com>
    Date:   Fri Aug 31 14:36:08 2018 +0200
    
        Allow for whitespace before header markers ***.
    
    commit ae61c25377f3959a13bd3f6d86efbed37bc7d7a1
    Author: PLG <xxx@plgd.xxx>
    Date:   Sun Sep 15 21:09:06 2019 +0200
    
        Creds. Change delimiter.

--- formatters/csv.php
+++ formatters/csv.php
@@ -1,24 +1,41 @@
 <?php
 // convert inline csv data into a table.
 // by OnegWR, may 2005, license GPL
+// http://wikkawiki.org/OnegWRCsv
+
+// Copy the code below into a file named formatters/csv.php
+// And give it the same file permissions as the other files in that directory.
+
 $csv_printheader=0;
-foreach(split("\n", $text) as $csv_n => $csv_line){
-        if(preg_match("/^#|^\s*$/",$csv_line)) continue;
-        if( $csv_printheader == 0){ print "<table cellpadding=\"5\" cellspacing=\"1\"><tbody>\n"; $csv_printheader=1; }
+foreach (split("\n", $text) as $csv_n => $csv_line) {
+        if(preg_match("/^#|^\s*$/",$csv_line)) 
+			continue;
+
+        if( $csv_printheader == 0) {
+			print "<table cellpadding=\"5\" cellspacing=\"1\"><tbody>\n"; $csv_printheader=1; 
+		}
         print ($csv_n%2) ? "<tr bgcolor=\"#ffffee\">" : "<tr bgcolor=\"#eeeeee\">";
-        foreach(split(";", $csv_line) as $csv_nn => $csv_cell){
-                if(preg_match("/^\s*$/",$csv_cell)){
+
+        foreach(split(",", $csv_line) as $csv_nn => $csv_cell) {
+                if(preg_match("/^\s*$/",$csv_cell)) {
                         print "<td>&nbsp;</td>";
-                }elseif(preg_match("/^(\"?)\*\*\*(.*?)\*\*\*(\"?)$/",$csv_cell,$matches)){
-                        print "<td bgcolor=\"#cccccc\">".
-                                $this->htmlspecialchars_ent($matches[2])."</td>";
-                }elseif(preg_match("/^(\"?)(.*?)(\"?)$/",$csv_cell,$matches)){
-                        print "<td>".$this->htmlspecialchars_ent($matches[2])."</td>";
-                }else{
+                } elseif(preg_match("/^\"?[\s]*==\/(.*?)\/==\"?$/",$csv_cell,$matches)) {
+                        print "<td style=\"background-color:#ccc; text-align:right;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+						$style[$csv_nn]= "text-align:right";
+                } elseif(preg_match("/^\"?[\s]*==\\\\(.*?)\\\\==\"?$/",$csv_cell,$matches)) {
+                        print "<td style=\"background-color:#ccc; text-align:left;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+						$style[$csv_nn]= "text-align:left";
+                } elseif(preg_match("/^\"?[\s]*==(.*?)==\"?$/",$csv_cell,$matches)) {
+                        print "<td style=\"background-color:#ccc; text-align:center;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+						$style[$csv_nn]= "text-align:center";
+                } elseif(preg_match("/^\"?(.*?)\"?$/",$csv_cell,$matches)) {
+                        print "<td style=\"".$style[$csv_nn]."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+                } else {
                         print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
                 }
         }
         print "</tr>\n";
 }
-if( $csv_printheader == 1) print "</tbody></table>\n";
+if ($csv_printheader == 1) print "</tbody></table>\n";
 ?>
+
