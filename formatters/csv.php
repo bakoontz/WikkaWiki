0fbbeae52454f952ae475d27a844b07b484a3fca
PLG <xxx@plgd.xxx>
2019-09-15 22:11:48 +0200

    Change delimiter again. Cleanup code. Independent test for headers.
    Squashed commit of the following:
    
    commit 0b52bc8fea9f9ef6f703303218b882a175244c9c
    Author: PLG <xxx@plgd.xxx>
    Date:   Sun Sep 15 21:31:45 2019 +0200
    
        Rename and spaces.
    
    commit d0f402786d5d5fb25566d714fd20e124b0f00a2b
    Author: PLG <kim@nevelsteen.com>
    Date:   Tue Aug 27 10:18:45 2019 +0200
    
        Remove redundannt question marks in regex.
    
    commit 6e0bfd62588c1d136af51c2b52a57fcf755968a4
    Author: PLG <kim@nevelsteen.com>
    Date:   Tue Aug 27 10:05:54 2019 +0200
    
        Independent test for headers.
    
    commit 87e3d3cc4bb3009bb4c604e813c1a847dd059b21
    Author: PLG <kim@nevelsteen.com>
    Date:   Mon Aug 26 23:33:26 2019 +0200
    
        Change else structure.
    
    commit 51825c23d02a5bc818c8cf0e477f3ce76a5e5728
    Author: PLG <kim@nevelsteen.com>
    Date:   Mon Aug 26 23:30:58 2019 +0200
    
        Change delimiter.
    
    commit 8743a1361d4b9a0038414185729395dd16c4f321
    Author: PLG <kim@nevelsteen.com>
    Date:   Mon Aug 26 23:30:16 2019 +0200
    
        Remove unnecessary flag.

--- formatters/csv.php
+++ formatters/csv.php
@@ -6,38 +6,42 @@
 // Copy the code below into a file named formatters/csv.php
 // And give it the same file permissions as the other files in that directory.
 
-$csv_printheader=0;
+print "<table><tbody>\n";
 foreach (split("\n", $text) as $csv_n => $csv_line) 
 {
 	if (preg_match("/^#|^\s*$/",$csv_line)) 
 		continue;
 
-	if ($csv_printheader == 0) {
-		print "<table cellpadding=\"5\" cellspacing=\"1\"><tbody>\n"; $csv_printheader=1; 
-	}
 	print ($csv_n%2) ? "<tr bgcolor=\"#ffffee\">" : "<tr bgcolor=\"#eeeeee\">";
 
-	foreach (split(",", $csv_line) as $csv_nn => $csv_cell) 
+	foreach (split(";", $csv_line) as $csv_nn => $csv_cell) 
 	{
-		if (preg_match("/^\s*$/",$csv_cell)) {
-			print "<td>&nbsp;</td>";
-		} elseif (preg_match("/^\"?[\s]*==\/(.*?)\/==\"?$/",$csv_cell,$matches)) {
-			print "<td style=\"background-color:#ccc; text-align:right;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-			$style[$csv_nn]= "text-align:right";
-		} elseif (preg_match("/^\"?[\s]*==\\\\(.*?)\\\\==\"?$/",$csv_cell,$matches)) {
-			print "<td style=\"background-color:#ccc; text-align:left;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-			$style[$csv_nn]= "text-align:left";
-		} elseif (preg_match("/^\"?[\s]*==(.*?)==\"?$/",$csv_cell,$matches)) {
-			print "<td style=\"background-color:#ccc; text-align:center;\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-			$style[$csv_nn]= "text-align:center";
-		} elseif (preg_match("/^\"?(.*?)\"?$/",$csv_cell,$matches)) {
-			print "<td style=\"".$style[$csv_nn]."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
-		} else {
-			print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
+		if (preg_match("/^\"?[\s]*==.*==\"?$/", $csv_cell)) 
+		{
+			$style[$csv_nn]= "padding: 1px 10px 1px 10px; ";
+
+			if (preg_match("/^\"?[\s]*==\/(.*)\/==\"?$/", $csv_cell, $title))
+				$style[$csv_nn].= "text-align:right; ";
+
+			elseif (preg_match("/^\"?[\s]*==\\\\(.*)\\\\==\"?$/", $csv_cell, $title))
+				$style[$csv_nn].= "text-align:left; ";
+
+			elseif (preg_match("/^\"?[\s]*==\|(.*)\|==\"?$/", $csv_cell, $title))
+				$style[$csv_nn].= "text-align:center; ";
+
+			print "<th style=\"background-color:#ccc;". $style[$csv_nn] ."\">". $this->htmlspecialchars_ent($title[1]) ."</th>";
+			continue;
 		}
+
+		if (preg_match("/^\s*$/",$csv_cell))
+			print "<td>&nbsp;</td>";
+		elseif (preg_match("/^\"?(.*)\"?$/", $csv_cell, $matches))
+			print "<td style=\"". $style[$csv_nn] ."\">".$this->htmlspecialchars_ent($matches[1])."</td>";
+		else
+			print "<td>".$this->htmlspecialchars_ent($csv_cell)."</td>";
 	}
 	print "</tr>\n";
 }
-if ($csv_printheader == 1) print "</tbody></table>\n";
+print "</tbody></table>\n";
 ?>
 
